407106e7c55c74415043f4949db0a300b79ea78c
David de la Iglesia <daviddelaiglesiacastro@gmail.com>
2016-10-18 17:29:21 +0200

    use PyntCloud in test

--- test/test_io.py
+++ test/test_io.py
@@ -1,9 +1,10 @@
 import os
 
-from pyntcloud.io.npz import read_npz, write_npz
-from pyntcloud.io.obj import read_obj, write_obj
-from pyntcloud.io.pcd import read_pcd, write_pcd
-from pyntcloud.io.ply import read_ply, write_ply
+from pyntcloud import PyntCloud
+from pyntcloud.io.ply import write_ply
+from pyntcloud.io.obj import write_obj
+from pyntcloud.io.npz import write_npz
+from pyntcloud.io.pcd import write_pcd
 
 # just in case test are being runned from other directory
 os.chdir(os.path.dirname(os.path.realpath(__file__)))
@@ -11,35 +12,35 @@ data_path = '../data/python'
 
 
 def assert_points_xyz(data):
-    assert data['points']['x'][23] == -1.5
-    assert data['points']['y'][23] == -5.5
-    assert data['points']['z'][23] == 1.5
+    assert data.points['x'][23] == -1.5
+    assert data.points['y'][23] == -5.5
+    assert data.points['z'][23] == 1.5
 
-    assert str(data['points']["x"].dtype) == 'float32'
-    assert str(data['points']["y"].dtype) == 'float32'
-    assert str(data['points']["z"].dtype) == 'float32'
+    assert str(data.points["x"].dtype) == 'float32'
+    assert str(data.points["y"].dtype) == 'float32'
+    assert str(data.points["z"].dtype) == 'float32'
     
 def assert_points_color(data):
-    assert data['points']['red'][23] == 0
-    assert data['points']['green'][23] == 170
-    assert data['points']['blue'][23] == 255
+    assert data.points['red'][23] == 0
+    assert data.points['green'][23] == 170
+    assert data.points['blue'][23] == 255
 
-    assert str(data['points']['red'].dtype) == 'uint8'
-    assert str(data['points']['green'].dtype) == 'uint8'
-    assert str(data['points']['blue'].dtype) == 'uint8'
+    assert str(data.points['red'].dtype) == 'uint8'
+    assert str(data.points['green'].dtype) == 'uint8'
+    assert str(data.points['blue'].dtype) == 'uint8'
 
 def assert_ply_mesh(data):
-    assert data["mesh"]["v1"][23] == 227
-    assert data["mesh"]["v2"][23] == 225
-    assert data["mesh"]["v3"][23] == 223
+    assert data.mesh["v1"][23] == 227
+    assert data.mesh["v2"][23] == 225
+    assert data.mesh["v3"][23] == 223
 
-    assert str(data['mesh']['v1'].dtype) == 'int32'
-    assert str(data['mesh']['v2'].dtype) == 'int32'
-    assert str(data['mesh']['v3'].dtype) == 'int32'
+    assert str(data.mesh['v1'].dtype) == 'int32'
+    assert str(data.mesh['v2'].dtype) == 'int32'
+    assert str(data.mesh['v3'].dtype) == 'int32'
 
 
 def test_read_ply_bin():
-    ply_bin = read_ply(data_path + '_bin.ply')
+    ply_bin = PyntCloud.from_file(data_path + '_bin.ply')
 
     assert_points_xyz(ply_bin)
     assert_points_color(ply_bin)
@@ -47,7 +48,7 @@ def test_read_ply_bin():
 
 
 def test_read_ply_ascii():
-    ply_ascii = read_ply(data_path + '.ply')
+    ply_ascii = PyntCloud.from_file(data_path + '.ply')
 
     assert_points_xyz(ply_ascii)
     assert_points_color(ply_ascii)
@@ -55,27 +56,27 @@ def test_read_ply_ascii():
     
     
 def test_write_ply():
-    data = read_ply(data_path + '_bin.ply')    
+    data = PyntCloud.from_file(data_path + '_bin.ply')    
     
-    write_ply(data_path + 'writed_ascii.ply', points=data["points"], mesh=data["mesh"],
-              comments=data["comments"], obj_info=data["obj_info"], as_text=True)  
-    write_ply(data_path + 'writed_bin.ply', points=data["points"], mesh=data["mesh"],
-              comments=data["comments"], obj_info=data["obj_info"], as_text=False) 
+    write_ply(data_path + 'writed_ascii.ply', points=data.points, mesh=data.mesh,
+              comments=data.comments, obj_info=data.obj_info, as_text=True)  
+    write_ply(data_path + 'writed_bin.ply', points=data.points, mesh=data.mesh,
+              comments=data.comments, obj_info=data.obj_info, as_text=False) 
               
-    writed_ply_ascii = read_ply(data_path + 'writed_ascii.ply')
-    writed_ply_bin = read_ply(data_path + 'writed_bin.ply')
+    writed_ply_ascii = PyntCloud.from_file(data_path + 'writed_ascii.ply')
+    writed_ply_bin = PyntCloud.from_file(data_path + 'writed_bin.ply')
     
-    assert all(data["points"] == writed_ply_ascii["points"])
-    assert all(data["points"] == writed_ply_bin["points"])
-    assert all(data["mesh"] == writed_ply_ascii["mesh"])
-    assert all(data["mesh"] == writed_ply_bin["mesh"])
+    assert all(data.points == writed_ply_ascii.points)
+    assert all(data.points == writed_ply_bin.points)
+    assert all(data.mesh == writed_ply_ascii.mesh)
+    assert all(data.mesh == writed_ply_bin.mesh)
 
     os.remove(data_path + 'writed_ascii.ply')
     os.remove(data_path + 'writed_bin.ply')
 
 
 def test_read_npz():
-    npz = read_npz(data_path + '.npz')
+    npz = PyntCloud.from_file(data_path + '.npz')
 
     assert_points_xyz(npz)
     assert_points_color(npz)
@@ -83,53 +84,53 @@ def test_read_npz():
     
     
 def test_write_npz():
-    data = read_ply(data_path + '_bin.ply')    
+    data = PyntCloud.from_file(data_path + '_bin.ply')    
 
-    write_npz(data_path + 'writed_npz.npz', points=data["points"], mesh=data["mesh"],
-              comments=data["comments"], obj_info=data["obj_info"])  
+    write_npz(data_path + 'writed_npz.npz', points=data.points, mesh=data.mesh,
+              comments=data.comments, obj_info=data.obj_info)  
 
-    writed_npz = read_npz(data_path + 'writed_npz.npz')
+    writed_npz = PyntCloud.from_file(data_path + 'writed_npz.npz')
 
-    assert all(data["points"] == writed_npz["points"])
-    assert all(data["mesh"] == writed_npz["mesh"])
+    assert all(data.points == writed_npz.points)
+    assert all(data.mesh == writed_npz.mesh)
 
     os.remove(data_path + 'writed_npz.npz')
 
 
 def test_read_obj():
-    obj = read_obj(data_path + '.obj')
+    obj = PyntCloud.from_file(data_path + '.obj')
     
     assert_points_xyz(obj)
 
 
 def test_write_obj():
-    data = read_ply(data_path + '_bin.ply')    
+    data = PyntCloud.from_file(data_path + '_bin.ply')    
     
-    write_obj(data_path + 'writed.obj', points=data["points"], mesh=data["mesh"],
-              comments=data["comments"], obj_info=data["obj_info"])  
+    write_obj(data_path + 'writed.obj', points=data.points, mesh=data.mesh,
+              comments=data.comments, obj_info=data.obj_info)  
 
-    writed_obj = read_obj(data_path + 'writed.obj')
+    writed_obj = PyntCloud.from_file(data_path + 'writed.obj')
     
-    assert all(data["points"][["x", "y", "z"]] == writed_obj["points"])
+    assert all(data.points[["x", "y", "z"]] == writed_obj.points)
     
     os.remove(data_path + 'writed.obj')
 
 
 def test_read_pcd():
-    pcd = read_pcd(data_path + '.pcd')
+    pcd = PyntCloud.from_file(data_path + '.pcd')
     
     assert_points_xyz(pcd)
     assert_points_color(pcd)
 
 
 def test_write_pcd():
-    data = read_ply(data_path + '_bin.ply')    
+    data = PyntCloud.from_file(data_path + '_bin.ply')    
     
-    write_pcd(data_path + 'writed.pcd', points=data["points"], comments=data["comments"])  
+    write_pcd(data_path + 'writed.pcd', points=data.points, comments=data.comments)  
 
-    writed_pcd = read_pcd(data_path + 'writed.pcd')
+    writed_pcd = PyntCloud.from_file(data_path + 'writed.pcd')
     
-    assert all(data["points"] == writed_pcd["points"])
+    assert all(data.points == writed_pcd.points)
     
     os.remove(data_path + 'writed.pcd')
 
