c64e560fda8a32033bce5a08284301bf92bbebea
David de la Iglesia <daviddelaiglesiacastro@gmail.com>
2016-11-28 20:08:21 +0100

    Octree.sizes added

--- pyntcloud/structures/octree.py
+++ pyntcloud/structures/octree.py
@@ -15,24 +15,21 @@ class Octree(object):
         self.structure = pd.DataFrame(np.zeros((self.points.shape[0], self.max_level), dtype=np.uint8))
         xyzmin = points.min(0)
         xyzmax = points.max(0)
-
         if bb_cuboid:
             #: adjust to obtain a  minimum bounding box with all sides of equal lenght 
             diff = max(xyzmax-xyzmin) - (xyzmax-xyzmin)
             xyzmin = xyzmin - diff / 2
             xyzmax = xyzmax + diff / 2
-
         self.xyzmin = xyzmin
         self.xyzmax = xyzmax
         self.id = "O({},{})".format(max_level, bb_cuboid)
-
+        self.sizes = [np.ptp([self.xyzmin, self.xyzmax], 0) / 2**i for i in range(1,self.max_level)]
         self.build()
-        
+
     def build(self):
         level_ptp = np.ptp([self.xyzmin, self.xyzmax], axis=0) / 2
         mid_points = np.zeros_like(self.points)
         mid_points[:] = (self.xyzmin + self.xyzmax) / 2
-
         for i in range(self.max_level):
             level_ptp /= 2
             bigger = self.points > mid_points
@@ -40,7 +37,7 @@ class Octree(object):
                 mid_points[:,j][bigger[:,j]] += level_ptp[j]
                 mid_points[:,j][~bigger[:,j]] -= level_ptp[j]
             bigger = bigger.astype(np.uint8)
-            self.structure.loc[:,i] = ((bigger[:,1] * 2) + bigger[:,0]) + (bigger[:,2] * (2 * 2))
+            self.structure.loc[:,i] = ((bigger[:,1] * 2) + bigger[:,0]) + (bigger[:,2] * (2 * 2))        
 
     def get_level_as_sf(self, level):
         sf = np.arange(len(self.points))
