16ddc061d323503054cac197148bbc787c8ddb91
David de la Iglesia <daviddelaiglesiacastro@gmail.com>
2016-11-28 20:53:53 +0100

    Octree a little faster

--- pyntcloud/structures/octree.py
+++ pyntcloud/structures/octree.py
@@ -9,33 +9,31 @@ import pandas as pd
 
 class Octree(object):
     
-    def __init__(self, points, max_level=2, bb_cuboid=True):
+    def __init__(self, points, max_level=2):
         self.points = points
         self.max_level= max_level
         self.structure = pd.DataFrame(np.zeros((self.points.shape[0], self.max_level), dtype=np.uint8))
         xyzmin = points.min(0)
         xyzmax = points.max(0)
-        if bb_cuboid:
-            #: adjust to obtain a  minimum bounding box with all sides of equal lenght 
-            diff = max(xyzmax-xyzmin) - (xyzmax-xyzmin)
-            xyzmin = xyzmin - diff / 2
-            xyzmax = xyzmax + diff / 2
+        #: adjust to obtain a  minimum bounding box with all sides of equal lenght 
+        diff = max(xyzmax-xyzmin) - (xyzmax-xyzmin)
+        xyzmin = xyzmin - diff / 2
+        xyzmax = xyzmax + diff / 2
         self.xyzmin = xyzmin
         self.xyzmax = xyzmax
-        self.id = "O({},{})".format(max_level, bb_cuboid)
-        self.sizes = [np.ptp([self.xyzmin, self.xyzmax], 0) / 2**i for i in range(1,self.max_level)]
+        self.id = "O({})".format(max_level)
         self.build()
 
     def build(self):
-        level_ptp = np.ptp([self.xyzmin, self.xyzmax], axis=0) / 2
+        self.sizes = np.zeros(self.max_level)
+        level_ptp = max(self.xyzmax-self.xyzmin) / 2
         mid_points = np.zeros_like(self.points)
         mid_points[:] = (self.xyzmin + self.xyzmax) / 2
         for i in range(self.max_level):
+            self.sizes[i] = level_ptp
             level_ptp /= 2
             bigger = self.points > mid_points
-            for j in range(3):                
-                mid_points[:,j][bigger[:,j]] += level_ptp[j]
-                mid_points[:,j][~bigger[:,j]] -= level_ptp[j]
+            mid_points = np.where(bigger, mid_points + level_ptp, mid_points - level_ptp)
             bigger = bigger.astype(np.uint8)
             self.structure.loc[:,i] = ((bigger[:,1] * 2) + bigger[:,0]) + (bigger[:,2] * (2 * 2))        
 
