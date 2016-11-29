7e1cf22c6b02f0cbd66839b5087ae491039623b8
David de la Iglesia <daviddelaiglesiacastro@gmail.com>
2016-11-29 13:23:50 +0100

    Octree faster get_level_as_sf

--- pyntcloud/structures/octree.py
+++ pyntcloud/structures/octree.py
@@ -9,10 +9,12 @@ import pandas as pd
 
 class Octree(object):
     
+class Octree(object):
+    
     def __init__(self, points, max_level=2):
         self.points = points
         self.max_level= max_level
-        self.structure = pd.DataFrame(np.zeros((self.points.shape[0], self.max_level), dtype=np.uint8))
+        self.structure = np.zeros((self.points.shape[0], self.max_level*3), dtype=bool)
         xyzmin = points.min(0)
         xyzmax = points.max(0)
         #: adjust to obtain a  minimum bounding box with all sides of equal lenght 
@@ -29,19 +31,21 @@ class Octree(object):
         level_ptp = max(self.xyzmax-self.xyzmin) / 2
         mid_points = np.zeros_like(self.points)
         mid_points[:] = (self.xyzmin + self.xyzmax) / 2
+        n = 0
         for i in range(self.max_level):
             self.sizes[i] = level_ptp
             level_ptp /= 2
             bigger = self.points > mid_points
-            copy = bigger.astype(np.uint8)
-            self.structure.loc[:,i] = ((copy[:,1] * 2) + copy[:,0]) + (copy[:,2] * (2 * 2))
+            self.structure[:, n:n+3] = bigger
             if i != self.max_level - 1:
-                mid_points = np.where(bigger, mid_points + level_ptp, mid_points - level_ptp)       
+                mid_points = np.where(bigger, mid_points + level_ptp, mid_points - level_ptp)
+                n += 3
 
     def get_level_as_sf(self, level):
         sf = np.arange(len(self.points))
+        st = pd.DataFrame(np.packbits(self.structure[:,:level*3], axis=1))
         i = 0
-        for g in self.structure.groupby([x for x in range(level)]).apply(lambda x: x.index.values).values:
+        for g in st.groupby([x for x in  st.columns]).apply(lambda x: x.index.values).values:
             sf[g] = i
             i+=1
 
