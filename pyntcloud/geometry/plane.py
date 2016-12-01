11ea692c682074ac77b40d620c34212458f3b629
David de la Iglesia <daviddelaiglesiacastro@gmail.com>
2016-12-01 20:18:49 +0100

    no classmethod in Plane

--- pyntcloud/geometry/plane.py
+++ pyntcloud/geometry/plane.py
@@ -5,21 +5,26 @@ import numpy as np
 class Plane():
 
     def __init__(self, point=None, normal=None):
-        self.point = point
-        self.normal= normal / np.linalg.norm(normal)
+        self.point = point 
+        if normal is not None:
+            self.normal= normal / np.linalg.norm(normal)
+        else:
+            self.normal = normal
         # for ransac
         self.k = 3
     
-    @classmethod
+
     def from_three_points(cls, points):
         normal = np.cross(points[1] - points[0], points[2] - points[0])
-        return cls(point=points[0], normal=normal)
+        self.point = points[0]
+        self.normal = normal / np.linalg.norm(normal)
 
-    @classmethod
+
     def from_equation(cls, a, b, c, d):
         normal = np.array([a,b,c])
         point = np.array([-d / a, -d / b, -d / c])
-        return cls(point=point, normal=normal)
+        self.point = point
+        self.normal = normal / np.linalg.norm(normal)
 
     def get_equation(self):
         a, b, c = self.normal
@@ -34,17 +39,10 @@ class Plane():
         projections = points - (distances[:,None] * self.normal)
         return distances, projections
 
-    def contains(self, a_point):
-        d1 = - np.dot(self.normal, self.point)
-        d2 = - np.dot(self.normal, a_point)
-        return True if d1 == d2 else False
-
     # RANSAC METHODS
-    
+
     def fit(self, points):
-        normal = np.cross(points[1] - points[0], points[2] - points[0])
-        self.point = points[0]
-        self.normal = normal / np.linalg.norm(normal)
+        self.from_three_points(points)
     
     def get_error(self, points):
         return self.get_projections(points, only_distances=True)     
