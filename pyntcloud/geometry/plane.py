7fe92df9d5b1074af249ca3df71493214e566c92
David de la Iglesia <daviddelaiglesiacastro@gmail.com>
2016-12-01 19:39:42 +0100

    Plane changes
    - from_three_points and from_equation now are class methods
    - contains method added
    - fit method changed
    - are_valid method added

--- pyntcloud/geometry/plane.py
+++ pyntcloud/geometry/plane.py
@@ -6,20 +6,20 @@ class Plane():
 
     def __init__(self, point=None, normal=None):
         self.point = point
-        self.normal= normal
+        self.normal= normal / np.linalg.norm(normal)
         # for ransac
         self.k = 3
     
-    def from_three_points(self, points):
+    @classmethod
+    def from_three_points(cls, points):
         normal = np.cross(points[1] - points[0], points[2] - points[0])
-        self.point = points[0]
-        self.normal = normal / np.linalg.norm(normal)
-    
-    def from_equation(self, a, b, c, d):
+        return cls(point=points[0], normal=normal)
+
+    @classmethod
+    def from_equation(cls, a, b, c, d):
         normal = np.array([a,b,c])
         point = np.array([-d / a, -d / b, -d / c])
-        self.point = point
-        self.normal = normal
+        return cls(point=point, normal=normal)
 
     def get_equation(self):
         a, b, c = self.normal
@@ -33,10 +33,25 @@ class Plane():
             return distances
         projections = points - (distances[:,None] * self.normal)
         return distances, projections
-        
+
+    def contains(self, a_point):
+        d1 = - np.dot(self.normal, self.point)
+        d2 = - np.dot(self.normal, a_point)
+        return True if d1 == d2 else False
+
+    # RANSAC METHODS
+    
     def fit(self, points):
-        self.from_three_points(points)
+        normal = np.cross(points[1] - points[0], points[2] - points[0])
+        self.point = points[0]
+        self.normal = normal / np.linalg.norm(normal)
     
     def get_error(self, points):
         return self.get_projections(points, only_distances=True)     
+    
+    def are_valid(self, points):
+        return True
+
+
+
 
