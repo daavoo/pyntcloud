a8918ec98fad9efd77418af696c45ec9e3cbcfe3
David de la Iglesia <daviddelaiglesiacastro@gmail.com>
2016-12-01 12:15:12 +0100

    optional kwarg filter in ransac

--- pyntcloud/ransac/ransac.py
+++ pyntcloud/ransac/ransac.py
@@ -6,14 +6,16 @@ Ransac Implementation
 
 import numpy as np
 
-def ransac(all_points, model, max_iterations=100, max_treshold=0):
+def ransac(points, model, filter=None, max_iterations=100, max_treshold=0):
+    if filter is not None:
+        points = points[filter]
     best_error = np.inf
     n_best_inliers = 0
         
     for i in range(max_iterations):
-        k_points = all_points[np.random.randint(len(all_points), size=model.k)]
+        k_points = points[np.random.randint(len(points), size=model.k)]
         model.fit(k_points)
-        test_error = model.get_error(all_points)
+        test_error = model.get_error(points)
         inliers = test_error <= max_treshold
         n_inliers = np.sum(inliers)
 
