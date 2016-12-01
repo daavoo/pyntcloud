9c55087bbe7adee10eefec4437e98b0c12f3fd63
David de la Iglesia <daviddelaiglesiacastro@gmail.com>
2016-12-01 12:22:03 +0100

    early_stop kwargs in ransac

--- pyntcloud/ransac/ransac.py
+++ pyntcloud/ransac/ransac.py
@@ -6,7 +6,14 @@ Ransac Implementation
 
 import numpy as np
 
-def ransac(points, model, filter=None, max_iterations=100, max_treshold=0):
+def ransac( points,
+            model,
+            filter=None,
+            max_iterations=100,
+            max_treshold=0.01,
+            inliers_stop=np.inf,
+            error_stop=0):
+            
     if filter is not None:
         points = points[filter]
     best_error = np.inf
@@ -15,13 +22,14 @@ def ransac(points, model, filter=None, max_iterations=100, max_treshold=0):
     for i in range(max_iterations):
         k_points = points[np.random.randint(len(points), size=model.k)]
         model.fit(k_points)
-        test_error = model.get_error(points)
-        inliers = test_error <= max_treshold
+        individual_error = model.get_error(points)
+        inliers = individual_error <= max_treshold
         n_inliers = np.sum(inliers)
 
         if n_inliers > n_best_inliers:
-            total_error = np.sum(test_error)
-
+            n_best_inliers = n_inliers
+            total_error = np.sum(individual_error)
+            
             if total_error < best_error:
                 best_error = total_error
                 best_inliers = inliers
