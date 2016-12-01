d3df850bf0fdfa02608cb57a758431047fbf27d2
David de la Iglesia <daviddelaiglesiacastro@gmail.com>
2016-12-01 12:07:42 +0100

    Plane class

--- pyntcloud/geometry/plane.py
+++ pyntcloud/geometry/plane.py
@@ -4,159 +4,39 @@
 import numpy as np
 
 
-def plane_def_by(three_points, equation=False):
-    """ Return the the plane defined by 3 points
-       
-    Parameters
-    ----------        
-    three_points: (3,3) ndarray
-        The x,y,z coordinates corresponding to the 3 points from which we want
-        to define the plane. Expected format:
-            array([[x1,y1,z1],
-                   [x2,y2,z2],
-                   [x3,y3,z3]])
-    
-    equation(Optional) : bool
-        Set the oputput plane format:
-            If True return the a,b,c,d coefficients of the plane.
-            If False(Default) return 1 Point and 1 Normal vector.
-            
-    Returns
-    -------
-    a, b, c, d : float
-        The coefficients solving the plane equation.
-        
-    or
-    
-    point, normal: (3,) ndarray
-        The plane defined by 1 Point and 1 Normal vector. With format:
-        array([Px,Py,Pz]), array([Nx,Ny,Nz])
+class Plane():
 
-    """
+    def __init__(self, point=None, normal=None):
+        self.point = point
+        self.normal= normal
     
-    #: define 2 vectors
-    v1 = three_points[1] - three_points[0]
-    v2 = three_points[2] - three_points[0]
+    def from_three_points(self, points):
+        normal = np.cross(points[1] - points[0], points[2] - points[0])
+        self.point = points[0]
+        self.normal = normal / np.linalg.norm(normal)
     
-    #: and 1 point
-    point = three_points[2]
-    
-    #: the normal of the plane is the cross product of the 2 vectors.
-    # normalized to obtain an unit normal.
-    normal = normalize(cross(v1,v2))
-    
-    if equation:
-        #: a,b,c are the x,y,z values of that normal vector
-        a, b, c = normal
-        
-        #: compute a*x + b*y + c*z which equals -d
-        d = - np.dot(normal, point)
-        
+    def from_equation(self, a, b, c, d):
+        normal = np.array([a,b,c])
+        point = np.array([-d / a, -d / b, -d / c])
+        self.point = point
+        self.normal = normal
+
+    def get_equation(self):
+        a, b, c = self.normal
+        d = - np.dot(self.normal, self.point)
         return a, b, c, d
+    
+    def get_projections(self, points, only_distances=False):
+        vectors = points - self.point
+        distances = np.dot(vectors, self.normal)
+        if only_distances:
+            return distances
+        projections = points - (distances[:,None] * self.normal)
+        return distances, projections
         
-    else:
-        return point, normal
-        
-
-@nb.jit(nopython=True)  
-def project_on_plane(points, plane_point, plane_normal, return_distances=False):
-    """ Return the projected points on the plane defined by plane_point and plane_normal.
+    def fit(self, points):
+        return self.from_three_points(points)
     
-    Parameters
-    ----------        
-    points: (N,3) ndarray
-        The x,y,z coordinates corresponding to the points which we want to project
-        on the given plane. Expected format:
-            array([[x1,y1,z1],
-                   ...
-                   [xn,yn,zn]])
-            
-    plane_point: (3,) ndarray
-        The x,y,z coordinates corresponding to a point which belong to the plane.
-        Expected format:
-            array([Px,Py,Pz])
-    
-    plane_normal: (3,) array
-        The x,y,z coordinates corresponding to the normal of the plane.
-        Expected format:
-            array([Nx,Ny,Nz])
-            
-    Returns
-    -------
-    proj_points : (N,3) array
-        The x,y,z coordinates corresponding to the points projected on the plane.
-        
-    """
-
-    #: just in case
-    plane_normal = normalize(plane_normal)
-        
-    #: get vectors from the plane's point to all the given test points
-    vectors = array_minus_vector(points, plane_point)
-    
-    #: get the scalar projection of the vectors onto the plane's normal
-    scalars = np.dot(vectors, plane_normal)
-    
-    #: get the orthogonal-projection vectors
-    orthogonal_projection = scalars_dot_vector(scalars, plane_normal)
-    
-    #: substract the projections to each point
-    proj_points = points - orthogonal_projection
-    
-    if return_distances:
-        return proj_points, norm_all(orthogonal_projection)
-    else:
-        return proj_points    
-        
-    
-def best_fitting_plane(points, equation=False):
-    """ Computes the best fitting plane of the given points
-    
-    Parameters
-    ----------        
-    points: array
-        The x,y,z coordinates corresponding to the points from which we want
-        to define the best fitting plane. Expected format:
-            array([
-            [x1,y1,z1],
-            ...,
-            [xn,yn,zn]])
-            
-    equation(Optional) : bool
-            Set the oputput plane format:
-                If True return the a,b,c,d coefficients of the plane.
-                If False(Default) return 1 Point and 1 Normal vector.    
-    Returns
-    -------
-    a, b, c, d : float
-        The coefficients solving the plane equation.
-    
-    or
-    
-    point, normal: array
-        The plane defined by 1 Point and 1 Normal vector. With format:
-        array([Px,Py,Pz]), array([Nx,Ny,Nz])
-        
-    """
-    #: compute PCA 
-    w, v = PCA(points)
-    
-    #: the normal of the plane is the last eigenvector
-    normal = normalize(v[:,2])
-       
-    #: get a point from the plane
-    point = np.mean(points, axis=0)
-    
-    #: if equation == True, return the a,b,c,d coefficients
-    if equation:
-        #: get A,B,C
-        a, b, c = normal
-        
-        #: get D
-        d = -(np.dot(normal, point))
-        return a, b, c, d
-        
-    #: else return the plane as a point and normal 
-    else:
-        return point, normal        
+    def get_error(self, points):
+        return self.get_projections(points, only_distances=True)     
 
