841332dd4b87cb17b575b48e46fb2a0d5c8001bc
David de la Iglesia <daviddelaiglesiacastro@gmail.com>
2016-12-02 18:48:54 +0100

    Ransac changes
    - Force copy of best model if return_best_model
    - Only use n_inliears to compare
    - Remove error_stop kwarg

--- pyntcloud/ransac/ransac.py
+++ pyntcloud/ransac/ransac.py
@@ -5,37 +5,35 @@ Ransac Implementation
 """
 
 import numpy as np
+import copy
 
 def ransac( points, model, filter=None, max_iterations=100,
-            treshold=0.01, inliers_stop=np.inf, error_stop=0,
-            return_model=False):
+            treshold=0.01, inliers_stop=np.inf, return_model=False):
 
     if filter is not None:
         points = points[filter]
 
-    best_error = np.inf
+    best_inliers = None
     n_best_inliers = 0        
     for i in range(max_iterations):
-        k_points = points[ np.random.randint(len(points), size=model.k) ]
 
+        k_points = points[ np.random.randint(len(points), size=model.k) ]
         if not model.are_valid(k_points):
             continue
 
         model.fit(k_points)
-        individual_error = model.get_error(points)
-        inliers = individual_error <= treshold
+        all_error = model.get_error(points)
+        inliers = all_error <= treshold
         n_inliers = np.sum(inliers)
-
+        
         if n_inliers > n_best_inliers:
             n_best_inliers = n_inliers
-            total_error = np.sum(individual_error)
-            
-            if total_error < best_error:
-                best_error = total_error
-                best_inliers = inliers
-    
+            best_inliers = inliers
+            if return_model:
+                best_model = copy.deepcopy(model)
+
     if return_model:
-        return best_inliers, model
-    
-    return best_inliers
+        return best_inliers, best_model
+    else:
+        return best_inliers
  
