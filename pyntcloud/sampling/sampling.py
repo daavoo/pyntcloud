8ab37f6188c8a675b058f3fe4dc585a749ba459d
David de la Iglesia <daviddelaiglesiacastro@gmail.com>
2017-01-09 20:13:36 +0100

    mesh_sampling slow version

--- pyntcloud/sampling/sampling.py
+++ pyntcloud/sampling/sampling.py
@@ -5,7 +5,22 @@ import numpy as np
 
 def random_sampling(points, n):
 
-    return points[np.random.choice(points.shape[0], size=n)]
+    return points[np.random.randint(0, points.shape[0], size=n)]
 
-def mesh_sampling(mesh, n):
-    pass
+
+def mesh_sampling(triangles, n):
+    
+    random_idx = np.random.randint(0, triangles.shape[0], size=n)
+    new_points = np.zeros((triangles.shape[0], triangles.shape[1]))
+
+    for i in range(len(random_idx)):
+        A, B, C = triangles[random_idx[i]]
+
+        u, v = np.random.rand(2)
+        if u + v > 1:
+            u = 1 - u
+            v = 1 - v
+
+        new_points[i] = (A * u) + (B * v) + ((1 - (u+v)) * C)
+    
+    return new_points
