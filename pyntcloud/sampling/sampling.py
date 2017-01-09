d1f7d87246e23da08c776852c6a932f87cd38d3f
David de la Iglesia <daviddelaiglesiacastro@gmail.com>
2017-01-09 12:45:34 +0100

    random_sampling added

--- pyntcloud/sampling/sampling.py
+++ pyntcloud/sampling/sampling.py
@@ -5,8 +5,5 @@ import numpy as np
 
 def random_sampling(points, size):
 
-    sampled = np.ones(points.shape[0], dtype=bool)
-
-    sampled[np.random.choice(points.shape[0], size=size)] = 0
-
-    return sampled
+    return points[np.random.choice(points.shape[0], size=size)]
+    
