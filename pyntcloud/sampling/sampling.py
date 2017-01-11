c46168a41ee604c8d1c1916b373b3e8f301ea773
David de la Iglesia <daviddelaiglesiacastro@gmail.com>
2017-01-11 11:36:14 +0100

    mesh_sampling probability adjusted
    Use the area of the triangles to make bigger triangles more likely to be
    selected and viceversa

--- pyntcloud/sampling/sampling.py
+++ pyntcloud/sampling/sampling.py
@@ -10,7 +10,10 @@ def random_sampling(points, n):
 
 def mesh_sampling(triangles, n):
     
-    random_idx = np.random.randint(0, triangles.shape[0], size=n)
+    areas = np.array([triangle_area(x) for x in triangles])
+    # bigger triangles -> more probability to be selected
+    probabilities = areas / np.sum(areas)
+    random_idx = np.random.choice(np.arange(len(areas)), size=n, p=probabilities)
     new_points = np.zeros((n, triangles.shape[1]))
 
     for i in range(len(random_idx)):
