90bd6bf259d8410f66e1546d8877154a41ebf941
David de la Iglesia <daviddelaiglesiacastro@gmail.com>
2017-01-24 12:12:13 +0100

    added read_off support to PyntCloud.from_file()

--- pyntcloud/io/__init__.py
+++ pyntcloud/io/__init__.py
@@ -8,13 +8,15 @@ from .npz import read_npz, write_npz
 from .obj import read_obj, write_obj
 from .pcd import read_pcd, write_pcd
 from .ply import read_ply, write_ply
+from .off import read_off
 
 FROM = {
 "MAT": read_mat,
 "NPZ": read_npz,
 "OBJ": read_obj,
 "PCD": read_pcd,
-"PLY": read_ply
+"PLY": read_ply,
+"OFF": read_off
 }
 
 TO = {
