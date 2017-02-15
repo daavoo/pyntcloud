6ee15a24853f9e9fdfaafceb7397997d69c3ba8c
David de la Iglesia <daviddelaiglesiacastro@gmail.com>
2017-02-15 12:28:07 +0100

    sampling __init__.py

--- pyntcloud/sampling/__init__.py
+++ pyntcloud/sampling/__init__.py
@@ -2,3 +2,13 @@
 """
 HAKUNA MATATA
 """
+
+from .sampling import *
+
+SAMPLE_POINTS = {
+    'random_sampling': random_sampling
+} 
+
+SAMPLE_MESH = {
+    'mesh_sampling': random_sampling
+} 
--- pyntcloud/scalar_fields/__init__.py
+++ pyntcloud/scalar_fields/__init__.py
@@ -6,52 +6,52 @@ HAKUNA MATATA
 from .scalar_fields import *
 
 SF_RANSAC = {
-'is_plane': is_plane,
-'is_sphere': is_sphere
+    'is_plane': is_plane,
+    'is_sphere': is_sphere
 }
 
 SF_NORMALS = {
-'inclination_deg': inclination_deg,
-'inclination_rad': inclination_rad,
-'orientation_deg': orientation_deg,
-'orientation_rad': orientation_rad,
+    'inclination_deg': inclination_deg,
+    'inclination_rad': inclination_rad,
+    'orientation_deg': orientation_deg,
+    'orientation_rad': orientation_rad,
 }
 
 SF_RGB = {
-'rgb_intensity' : (['Ri', 'Gi', 'Bi'], rgb_intensity),
-'relative_luminance': relative_luminance,
-'hsv' : (['H', 'S', 'V'], hsv)
+    'rgb_intensity' : (['Ri', 'Gi', 'Bi'], rgb_intensity),
+    'relative_luminance': relative_luminance,
+    'hsv' : (['H', 'S', 'V'], hsv)
 }
 
 SF_OCTREE = {
-'octree_level': octree_level,
-'eigen_octree': (['e1', 'e2', 'e3'], eigen_octree),
-'eigen_full_octree': (['e1', 'e2', 'e3', 'ev1', 'ev2', 'ev3'], eigen_full_octree)
+    'octree_level': octree_level,
+    'eigen_octree': (['e1', 'e2', 'e3'], eigen_octree),
+    'eigen_full_octree': (['e1', 'e2', 'e3', 'ev1', 'ev2', 'ev3'], eigen_full_octree)
 }
 
 SF_VOXELGRID = {
-'voxel_x': voxel_x,
-'voxel_y': voxel_y,
-'voxel_z': voxel_z,
-'voxel_n': voxel_n,
-'eigen_voxelgrid': (['e1', 'e2', 'e3'], eigen_voxelgrid),
-'eigen_full_voxelgrid': (['e1', 'e2', 'e3', 'ev1', 'ev2', 'ev3'], eigen_full_voxelgrid)
+    'voxel_x': voxel_x,
+    'voxel_y': voxel_y,
+    'voxel_z': voxel_z,
+    'voxel_n': voxel_n,
+    'eigen_voxelgrid': (['e1', 'e2', 'e3'], eigen_voxelgrid),
+    'eigen_full_voxelgrid': (['e1', 'e2', 'e3', 'ev1', 'ev2', 'ev3'], eigen_full_voxelgrid)
 }
 
 SF_KDTREE = {
-'eigen_kdtree': (['e1', 'e2', 'e3'], eigen_kdtree),
-'eigen_full_kdtree': (['e1', 'e2', 'e3', 'ev1', 'ev2', 'ev3'], eigen_full_kdtree)
+    'eigen_kdtree': (['e1', 'e2', 'e3'], eigen_kdtree),
+    'eigen_full_kdtree': (['e1', 'e2', 'e3', 'ev1', 'ev2', 'ev3'], eigen_full_kdtree)
 }
 
 SF_EIGENVALUES = {
-'eigen_sum': eigen_sum,
-'omnivariance': omnivariance,
-'eigenentropy': eigenentropy,
-'anisotropy': anisotropy,
-'planarity': planarity,
-'linearity': linearity,
-'curvature': curvature,
-'sphericity': sphericity
+    'eigen_sum': eigen_sum,
+    'omnivariance': omnivariance,
+    'eigenentropy': eigenentropy,
+    'anisotropy': anisotropy,
+    'planarity': planarity,
+    'linearity': linearity,
+    'curvature': curvature,
+    'sphericity': sphericity
 }
 
 ALL_SF = "".join([" {}"] * 7).format(
