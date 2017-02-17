d0062237bd34d277167bfabe3585fab4fc612b31
David de la Iglesia <daviddelaiglesiacastro@gmail.com>
2017-02-17 12:24:53 +0100

    Simplifying io - MAT

--- pyntcloud/io/mat.py
+++ pyntcloud/io/mat.py
@@ -4,7 +4,9 @@ import pandas as pd
 from scipy.io import loadmat, savemat
 
 
-def read_mat(filename):
+def read_mat(filename, points_name="points", mesh_name="mesh", 
+            points_columns="points_columns",
+            mesh_columns="mesh_columns"):
     """ Read a .mat file and store all possible elements in pandas DataFrame 
     Parameters
     ----------
@@ -17,37 +19,22 @@ def read_mat(filename):
         If possible, elements as pandas DataFrames else input format
     """
     
-    mat = loadmat(filename)
+    data = loadmat(filename)
 
-    if "points" in mat:
-        columns = None
-
-        if "points_names" in mat:
-            columns= [mat["points_names"][i].strip() for i in range(len(mat["points_names"]))]
-            del mat["points_names"]
-        
-        mat["points"] = pd.DataFrame(mat["points"], columns=columns)
-
-        if "points_dtypes" in mat:
-            for i in range(len(mat["points_dtypes"])):
-                mat["points"][columns[i]] = mat["points"][columns[i]].astype(mat["points_dtypes"][i].strip())
-            
-            del mat["points_dtypes"]
-            
     
-    if "mesh" in mat:
+    if points_columns in data:
+        columns = [data[points_columns][i].strip() for i in range(len(data[points_columns]))]
+        del data[points_columns]
+        
+        data[points_name] = pd.DataFrame(data[points_name], columns=columns)            
+    
+    if mesh_name in data:
         columns = None
 
-        if "mesh_names" in mat:
-            columns= [mat["mesh_names"][i].strip() for i in range(len(mat["mesh_names"]))]
-            del mat["mesh_names"]
+        if mesh_columns in data:
+            columns= [data[mesh_columns][i].strip() for i in range(len(data[mesh_columns]))]
+            del data[mesh_columns]
         
-        mat["mesh"] = pd.DataFrame(mat["mesh"], columns=columns)
-
-        if "mesh_dtypes" in mat:
-            for i in range(len(mat["mesh_dtypes"])):
-                mat["mesh"][columns[i]] = mat["mesh"][columns[i]].astype(mat["mesh_dtypes"][i].strip())
+        data[mesh_name] = pd.DataFrame(data[mesh_name], columns=columns)
             
-            del mat["mesh_dtypes"]
-            
-    return mat
+    return data
