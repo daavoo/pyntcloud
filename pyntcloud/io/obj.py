c0835b87204c26f98c21acfe0660394c1da64871
David de la Iglesia <daviddelaiglesiacastro@gmail.com>
2017-02-17 20:32:08 +0100

    ignore comments in read_obj

--- pyntcloud/io/obj.py
+++ pyntcloud/io/obj.py
@@ -16,19 +16,13 @@ def read_obj(filename):
     Each obj element found as pandas Dataframe.
 
     """
-    comments = []
-    obj_info = []
     v = []
     vn = []
     f = []
     
     with open(filename) as obj:
         for line in obj:
-            
-            if line.startswith('#'):
-                comments.append(line.strip()[1:])
-                
-            elif line.startswith('v '):
+            if line.startswith('v '):
                 v.append(line.strip()[1:].split())
                 
             elif line.startswith('vn'):
@@ -52,12 +46,12 @@ def read_obj(filename):
     
     mesh = pd.DataFrame(f, dtype='i4', columns=mesh_columns)
     
-    data = {'comments': comments, 'obj_info': obj_info, 'points': points, 'mesh': mesh, "normals":vn}
+    data = {'points': points, 'mesh': mesh, "normals":vn}
     
     return data
             
 
-def write_obj(filename, points=None, mesh=None, comments=[], obj_info=[]):
+def write_obj(filename, points=None, mesh=None):
     """
     Parameters
     ----------
@@ -65,8 +59,6 @@ def write_obj(filename, points=None, mesh=None, comments=[], obj_info=[]):
         The created file will be named with this
     points:     pd.DataFrame
     mesh:       pd.DataFrame
-    comments:   list[str] or str
-    obj_info:   list[str] or str
 
     Returns
     -------
@@ -76,14 +68,6 @@ def write_obj(filename, points=None, mesh=None, comments=[], obj_info=[]):
     """     
     if not filename.endswith('obj'):
         filename += '.obj'
-
-    with open(filename, "w") as obj:
-
-        for line in comments:
-            obj.write("#%s\n" % line.strip())
-
-        for line in obj_info:
-            obj.write("#%s\n" % line.strip())
 
     if points is not None:
         # because we don't want the insert on the original data
