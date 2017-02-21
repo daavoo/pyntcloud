3b77a38d79202efbfa69e553267fc65459a8e31c
David de la Iglesia <daviddelaiglesiacastro@gmail.com>
2017-02-21 20:21:31 +0100

    deal with nan values in rgb_intensity

--- pyntcloud/scalar_fields/scalar_fields.py
+++ pyntcloud/scalar_fields/scalar_fields.py
@@ -37,7 +37,7 @@ def orientation_rad(normals):
 # NEED RGB
 
 def rgb_intensity(rgb):
-    rgb_i = rgb / np.sum(rgb, axis=1, keepdims=True) 
+    rgb_i = np.nan_to_num(rgb / np.sum(rgb, axis=1, keepdims=True)) 
     return rgb_i[:,0], rgb_i[:,1], rgb_i[:,2]
 
 def relative_luminance(rgb):
