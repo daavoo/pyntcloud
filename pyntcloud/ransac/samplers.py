79828671849021a32333944513b512d5200b2001
David de la Iglesia <daviddelaiglesiacastro@gmail.com>
2017-03-06 20:45:47 +0100

    Added ransac/samplers.py
    Module to add flexibility to ransac.
    - Added base class Ransac_Sampler(ABC)
    - Added Random_Sampler(Ransac_Sampler).
    - Added Voxelgrid_Sampler(Ransac_Sampler).

---
+++ pyntcloud/ransac/samplers.py
@@ -0,0 +1,76 @@
+import numpy as np
+
+from abc import ABC, abstractmethod
+from structures import VoxelGrid
+
+class Ransac_Sampler(ABC):
+    """ Base class for ransac samplers.
+
+    Parameters
+    ----------
+    points : ndarray
+        (N, M) ndarray where N is the number of points and M is the number
+        scalar fields associated to each of those points. 
+        M is usually 3 for representing the x, y, and z coordinates of each point.
+
+    k : int
+        The number of points that will be sampled in each call of get_sample().
+        This number depends on the model used. See ransac/models.py.
+    """
+    def __init__(self, points, k):
+        self.points = points
+        self.k = k
+
+    @abstractmethod
+    def get_sample(self):
+        pass
+
+
+class Random_Sampler(Ransac_Sampler):
+    """ Sample random points.
+
+    Inherits from Ransac_Sampler.
+
+    """
+    def __init__(self, points, k):
+        super().__init__(points, k)
+    
+    def get_sample(self):
+        """ Get k unique random points.
+        """
+        sample = np.random.choice(len(self.points), self.k, replace=False)
+        return self.points[sample]
+        
+
+class Voxelgrid_Sampler(Ransac_Sampler):
+    """ Sample random points inside the same random voxel.
+
+    Inherits from Ransac_Sampler.
+
+    Parameters
+    ----------
+    voxel_size : float, optional (default 0.1)
+
+    """
+    def __init__(self, points, k, voxel_size=0.1):
+        super().__init__(points, k)
+        sizes = [voxel_size] * 3
+        self.voxelgrid = VoxelGrid(self.points, sizes=sizes, bb_cuboid=False)
+    
+    def get_sample(self):
+        """ Get k unique random points from the same voxel of one randomly picked point.
+        """
+        # pick one point and get it's voxel index 
+        idx = random.randint(0, len(self.points))
+        voxel = self.voxelgrid.voxel_n[idx]
+
+        # get index of points inside that voxel and convert to probabilities
+        points_in_voxel = (self.voxelgrid.voxel_n == voxel).astype(int)
+        points_in_voxel = points_in_voxel / points_in_voxel.sum()
+
+        sample = np.random.choice(len(self.points), self.k, replace=False, p=points_in_voxel)
+        
+        return self.points[sample]
+
+        
+        
