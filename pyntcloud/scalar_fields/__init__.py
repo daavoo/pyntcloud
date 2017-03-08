e1ac27f32fc45ae839b9ff7bafcb4ac33a7a9743
David de la Iglesia <daviddelaiglesiacastro@gmail.com>
2017-03-08 20:06:20 +0100

    rewriting scalar_fields/__init__.py
    Now all sf are grouped in ALL_SF dict.

--- pyntcloud/scalar_fields/__init__.py
+++ pyntcloud/scalar_fields/__init__.py
@@ -1,76 +1,69 @@
-
-"""
-HAKUNA MATATA
-"""
-
 from .sf_eigenvalues import (
-    anisotropy,
-    curvature,
-    eigenentropy,
-    eigen_sum,
-    linearity,
-    omnivariance,
-    planarity,    
-    sphericity
+    Anisotropy,
+    Curvature,
+    Eigenentropy,
+    EigenSum,
+    Linearity,
+    Omnivariance,
+    Planarity,    
+    Sphericity
 )
-from .sf_k_neighbors import (
-    eigen_decomposition,
-    eigen_values
+from .sf_kneighbors import (
+    EigenDecomposition,
+    EigenValues
 )
 from .sf_normals import (
-    inclination_deg,
-    inclination_rad,
-    orientation_deg,
-    orientation_rad
+    InclinationDegrees,
+    InclinationRadians,
+    OrientationDegrees,
+    OrientationRadians
+)
+from .sf_ransac import (
+    PlaneFit,
+    SphereFit,
+    CustomFit
 )
 from .sf_rgb import (
-    hsv,
-    relative_luminance,
-    rgb_intensity
+    HSV,
+    RelativeLuminance,
+    RGBIntensity
 )
 from .sf_voxelgrid import (
-    voxel_n,
-    voxel_x,
-    voxel_y,
-    voxel_z
-)
-from .sf_xyz import (
-    is_plane,
-    is_sphere
+    VoxelN,
+    VoxelX,
+    VoxelY,
+    VoxelZ
 )
 
-SF_EIGENVALUES = {
-    'anisotropy': (['anisotropy'], anisotropy),
-    'curvature': (['curvature'], curvature),
-    'eigenentropy': (['eigenentropy'], eigenentropy),
-    'eigen_sum': (['eigen_sum'], eigen_sum),
-    'linearity': (['linearity'], linearity),
-    'omnivariance': (['omnivariance'], omnivariance),    
-    'planarity': (['planarity'] , planarity),  
-    'sphericity': (['sphericity'], sphericity)
-}
-SF_K_NEIGHBORS = {
-    'eigen_decomposition': (['e1', 'e2', 'e3', 'ev1', 'ev2', 'ev3'], eigen_decomposition) ,
-    'eigen_values': (['e1', 'e2', 'e3'], eigen_values)
-}
-SF_NORMALS = {
-    'inclination_deg': (['inclination_deg'], inclination_deg),
-    'inclination_rad': (['inclination_rad'], inclination_rad),
-    'orientation_deg': (['orientation_deg'], orientation_deg),
-    'orientation_rad': (['orientation_rad'],orientation_rad),
-}
-SF_RGB = {
-    'hsv' : (['H', 'S', 'V'], hsv),
-    'relative_luminance': (["relative_luminance"], relative_luminance),
-    'rgb_intensity' : (['Ri', 'Gi', 'Bi'], rgb_intensity)    
-}
-SF_VOXELGRID = {
-    'voxel_n': (['voxel_n'], voxel_n),
-    'voxel_x': (['voxel_x'], voxel_x),
-    'voxel_y': (['voxel_y'], voxel_y),
-    'voxel_z': (['voxel_z'], voxel_z)
-}
-SF_XYZ = {
-    'is_plane': (["is_plane"], is_plane),
-    'is_sphere': (["is_sphere"], is_sphere)
+ALL_SF = {
+    # Eigenvalues
+    'anisotropy': Anisotropy,
+    'curvature': Curvature,
+    'eigenentropy': Eigenentropy,
+    'eigen_sum': EigenSum,
+    'linearity': Linearity,
+    'omnivariance': Omnivariance,    
+    'planarity': Planarity, 
+    'sphericity': Sphericity,
+    # Kneighbors
+    'eigen_decomposition': EigenDecomposition,
+    'eigen_values': EigenValues, 
+    # Normals
+    'inclination_deg': InclinationDegrees,
+    'inclination_rad': InclinationRadians,
+    'orientation_deg': OrientationDegrees,
+    'orientation_rad': OrientationRadians,
+    # Ransac
+    'custom_fit': CustomFit,
+    'plane_fit': PlaneFit,
+    'sphere_fit': SphereFit,
+    # RGB
+    'hsv': HSV,
+    'relative_luminance': RelativeLuminance,
+    'rgb_intensity': RGBIntensity,
+    # Voxelgrid
+    'voxel_n': VoxelN,
+    'voxel_x': VoxelX,
+    'voxel_y': VoxelY,
+    'voxel_z': VoxelZ
 }
