8859b898fd999509355b998666c3d2f36101cb50
David de la Iglesia <daviddelaiglesiacastro@gmail.com>
2017-03-09 11:55:53 +0100

    filtetrs/base.py added

---
+++ pyntcloud/filters/base.py
@@ -0,0 +1,17 @@
+from abc import ABC, abstractmethod
+
+class Filter(ABC):
+    """ Base class for filters.
+    """
+
+    def __init__(self, pyntcloud):
+        self.pyntcloud = pyntcloud
+
+    @abstractmethod
+    def extract_info(self):
+        pass
+    
+    @abstractmethod
+    def compute(self):
+        pass
+
