389fa6b4472470b9ad0521ef09c09e68714d0c1e
David de la Iglesia Castro <daviddelaiglesiacastro@gmail.com>
2017-03-29 14:11:43 +0200

    added empty io/pcd.py

---
+++ pyntcloud/io/pcd.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+
+def read_pcd(filename):
+    """ Reads and pcd file and return the elements as pandas Dataframes.
+
+    Parameters
+    ----------
+    filename: str
+        Path to the obj file.
+
+    Returns
+    -------
+    Each pcd element found as pandas Dataframe.
+
+    """
+    return
