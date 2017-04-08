e299362c55c3f6ba34c37fb195ba0f0eb16fffe3
David de la Iglesia Castro <daviddelaiglesiacastro@gmail.com>
2017-04-08 15:32:10 +0200

    Adapting ransac scalar fields to new kwarg

--- pyntcloud/scalar_fields/sf_xyz.py
+++ pyntcloud/scalar_fields/sf_xyz.py
@@ -1,3 +1,4 @@
+import numpy as np
 from ..ransac import single_fit, RANSAC_MODELS, RANSAC_SAMPLERS
 from .base import ScalarField
 
@@ -21,35 +22,41 @@ class ScalarField_XYZ(ScalarField):
 class PlaneFit(ScalarField_XYZ):
     """ Points belonging to the best RansacPlane found.
     """
-    def __init__(self, pyntcloud, max_dist=1e-4, max_iterations=100):
+    def __init__(self, pyntcloud, max_dist=1e-4, max_iterations=100, n_inliers_to_stop=None):
         self.model = RANSAC_MODELS["plane"]
         self.sampler = RANSAC_SAMPLERS["random"]
         self.name = "is_plane"
         self.model_kwargs = {"max_dist" : max_dist}
         self.max_iterations = max_iterations
+        self.n_inliers_to_stop = n_inliers_to_stop
+        
         super().__init__(pyntcloud)
         
     def compute(self):
         self.to_be_added[self.name] =  single_fit(self.points, self.model, self.sampler,
                                          model_kwargs=self.model_kwargs,
-                                         max_iterations=self.max_iterations)
+                                         max_iterations=self.max_iterations,
+                                         n_inliers_to_stop=self.n_inliers_to_stop).astype(np.uint8)
 
 class SphereFit(ScalarField_XYZ):
     """ Returns wich points belong to the best RansacSphere found.
 
     """
-    def __init__(self, pyntcloud, max_dist=1e-4, max_iterations=100):
+    def __init__(self, pyntcloud, max_dist=1e-4, max_iterations=100, n_inliers_to_stop=None):
         self.model = RANSAC_MODELS["sphere"]
         self.sampler = RANSAC_SAMPLERS["random"]
         self.name = "is_sphere"
         self.model_kwargs = {"max_dist" : max_dist}
         self.max_iterations = max_iterations
+        self.n_inliers_to_stop = n_inliers_to_stop
+        
         super().__init__(pyntcloud)
         
     def compute(self):
         self.to_be_added[self.name] =  single_fit(self.points, self.model, self.sampler,
                                          model_kwargs=self.model_kwargs,
-                                         max_iterations=self.max_iterations)
+                                         max_iterations=self.max_iterations,
+                                         n_inliers_to_stop=self.n_inliers_to_stop).astype(np.uint8)
 
 class CustomFit(ScalarField_XYZ):
     """ Fit using custom model and/or sampler
