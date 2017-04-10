89943ee4d01685804dfa2e289eae96b094966f2a
David de la Iglesia Castro <daviddelaiglesiacastro@gmail.com>
2017-04-10 16:17:56 +0200

    Added utils/numba

---
+++ pyntcloud/utils/numba.py
@@ -0,0 +1,20 @@
+from numba import jit
+
+@jit
+def groupby_count(xyz, indices, out):
+    for i in range(xyz.shape[0]):
+        out[indices[i]] += 1
+    return out
+
+@jit
+def groupby_sum(xyz, indices, N, out):
+    for i in range(xyz.shape[0]):
+        out[indices[i]] += xyz[i][N]
+    return out
+
+@jit
+def groupby_max(xyz, indices, N, out):
+    for i in range(xyz.shape[0]):
+        if xyz[i][N] > out[indices[i]]:
+            out[indices[i]] = xyz[i][N]    
+    return out
