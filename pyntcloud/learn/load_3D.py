71f50ac02d7c4735526b3fe0b67a823b1c87a922
David de la Iglesia Castro <daviddelaiglesiacastro@gmail.com>
2017-04-17 12:18:32 +0200

    fixes in learn module merge

--- pyntcloud/learn/load_3D.py
+++ pyntcloud/learn/load_3D.py
@@ -1,7 +1,7 @@
 from ..pyntcloud import PyntCloud
 
 def load_3D(path, 
-            n_sampling=None,
+            n_sampling=10000,
             voxelize=True,
             voxel_mode="binary",
             target_size=(30,30,30)):
@@ -11,19 +11,24 @@ def load_3D(path,
     ----------
     path : srt
         Path to 3D file.
+        
     n_sampling : int
         Number of points to be sampled in case the readed 3D data contains a mesh.
+        
     voxelize : bool, optional (Default True)
         Indicates wheter the 3D data will be converted into voxelgrid or not.
-    voxel_mode : {"binary", "density", "tdf"}, optional (Default "binary")
+        
+    voxel_mode : {"binary", "density", "TDF"}, optional (Default "binary")
         The type of feature vector that will be generated from the voxelgrid.
-        binary : uint8
-            0 for unnocupied voxels 1 for occupied
-        density : float16
-            n_points_in_voxel / n_total_points
-        truncated : float16
-            Value between 0 and 1 indicating the distance between the voxel's center and
-            the closest point. 1 on the surface, 0 on voxels further than 2 * voxel side.            
+        binary
+            0 for empty voxels, 1 for occupied.            
+        density
+            number of points inside voxel / total number of points.            
+        TDF
+            Truncated Distance Function. Value between 0 and 1 indicating the distance 
+            between the voxel's center and the closest point. 1 on the surface,
+            0 on voxels further than 2 * voxel side.            
+            
     target_size : [int, int, int], optional (Default [30, 30, 30])
         Dimensions of voxelgrid in case voxelize is True.
 
@@ -40,24 +45,21 @@ def load_3D(path,
     
     point_cloud = PyntCloud.from_file(path)
     
-    if "mesh" in point_cloud:
-        if n_sampling is None:
-            n_sampling = len(point_cloud["mesh"]) * 10
-
+    if point_cloud.mesh is not None:
         point_cloud = PyntCloud(point_cloud.get_sample("mesh_random_sampling", n=n_sampling))
     
     if voxelize:
         vgrid_id = point_cloud.add_structure("voxelgrid", x_y_z=target_size)
-        voxelgrid = point_cloud.structures[vgrid_id]
+        voxelgrid = point_cloud.voxelgrids[vgrid_id]
         
         if voxel_mode == "binary":
-            feature_vector = binary_vector(v_grid, target_size)
+            feature_vector = voxelgrid.get_feature_vector(mode="binary")
         elif voxel_mode == "density":
-            feature_vector = density_vector(v_grid, target_size)
-        elif voxel_mode == "truncated":
-            feature_vector = truncated_distance_function(point_cloud, centers, target_size, dimensions)
+            feature_vector = voxelgrid.get_feature_vector(mode="density")
+        elif voxel_mode == "TDF":
+            feature_vector = voxelgrid.get_feature_vector(mode="density")
         else:
-            raise ValueError("Unvalid mode; avaliable modes are: {}".format({"binary", "density", "truncated"}))
+            raise ValueError("Unvalid mode; avaliable modes are: {}".format({"binary", "density", "TDF"}))
         
         return feature_vector
         
--- pyntcloud/learn/models/__init__.py
+++
@@ -1,2 +0,0 @@
-from .voxnet import VoxNet
-
