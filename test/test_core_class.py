c0d2e9a63a70939473929934babb0327367562d2
David de la Iglesia Castro <daviddelaiglesiacastro@gmail.com>
2017-04-21 12:24:04 +0200

    test_clean_all_structures in test_core_class

--- test/test_core_class.py
+++ test/test_core_class.py
@@ -4,6 +4,10 @@ import pandas as pd
 from pyntcloud import PyntCloud
 
 def test_points():
+    """
+    - Points must be a pandas DataFrame
+    - DataFrame must have at least "x", "y" and "z" named columns
+    """
     points = np.random.rand(10, 3)
     
     # not dataframe
@@ -19,4 +23,24 @@ def test_points():
     points = pd.DataFrame(points.values, columns=["x","y","z"])
     
     assert PyntCloud(points)
-
+    
+def test_clean_all_structures():
+    """
+    - When PyntCloud.points is re-assigned all structures must be removed
+    """
+    
+    points = np.random.rand(10, 3)
+    points = pd.DataFrame(points, columns=["x","y","z"])
+    
+    cloud = PyntCloud(points)
+    
+    cloud.add_structure("voxelgrid")
+    
+    assert len(cloud.voxelgrids) == 1
+    
+    # dummy filter
+    x_above_05 = cloud.points["x"] > 0.5
+    cloud.points = cloud.points[x_above_05]
+    
+    assert len(cloud.voxelgrids) == 0
+    
