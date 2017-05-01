9d4b0411a3729cc1a8e7463c48fd2d03eb630074
David de la Iglesia Castro <daviddelaiglesiacastro@gmail.com>
2017-05-01 14:35:52 +0200

    try if this fix readthedocs

--- docs/conf.py
+++ docs/conf.py
@@ -19,7 +19,8 @@
 
 import os
 import sys
-sys.path.append(os.path.dirname(os.path.dirname(__file__)))
+
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 # -- General configuration ------------------------------------------------
 
