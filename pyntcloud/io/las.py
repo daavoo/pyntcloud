2a89428e4f474794e238ce0115c22a13294357a6
David de la Iglesia Castro <daviddelaiglesiacastro@gmail.com>
2017-05-04 19:03:45 +0200

    read_las now also returns laspy.Header
    - Add laspytest/data files

--- pyntcloud/io/las.py
+++ pyntcloud/io/las.py
@@ -3,7 +3,8 @@ import pandas as pd
 
 
 def read_las(filename):
-    """ Read a .las/laz file and store elements in pandas DataFrame
+    """Read a .las/laz file and store elements in pandas DataFrame.
+
     Parameters
     ----------
     filename: str
@@ -13,11 +14,11 @@ def read_las(filename):
     data: dict
         Elements as pandas DataFrames.
     """
-
     data = {}
 
     with laspy.file.File(filename) as las:
         data["points"] = pd.DataFrame(las.points["point"])
         data["points"].columns = (x.lower() for x in data["points"].columns)
+        data["las_header"] = las.header
 
     return data
