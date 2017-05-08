16463f1594953774f1bfcac86d11e70c28d6c0c2
David de la Iglesia Castro <daviddelaiglesiacastro@gmail.com>
2017-05-08 13:58:50 +0200

    VoxNet changes
    - Add optional kwargs for loading weights
    - Head use Ordered_dict
    - Fix stride in conv2

--- pyntcloud/learn/models/voxnet.py
+++ pyntcloud/learn/models/voxnet.py
@@ -12,14 +12,17 @@ from collections import OrderedDict
 
 class VoxNet(torch.nn.Module):
 
-    def __init__(self, num_classes=10, input_shape=(32, 32, 32)):
+    def __init__(self, num_classes=10, input_shape=(32, 32, 32),
+                 weights_path=None,
+                 load_body_weights=True,
+                 load_head_weights=True):
 
         super().__init__()
         self.body = torch.nn.Sequential(OrderedDict([
             ('conv1', torch.nn.Conv3d(in_channels=1, out_channels=32, kernel_size=5, stride=2)),
             ('lkrelu1', torch.nn.LeakyReLU()),
             ('drop1', torch.nn.Dropout(p=0.2)),
-            ('conv2', torch.nn.Conv3d(in_channels=32, out_channels=32, kernel_size=3, stride=2)),
+            ('conv2', torch.nn.Conv3d(in_channels=32, out_channels=32, kernel_size=3)),
             ('lkrelu2', torch.nn.LeakyReLU()),
             ('pool2', torch.nn.MaxPool3d(2)),
             ('drop2', torch.nn.Dropout(p=0.3))
@@ -31,12 +34,20 @@ class VoxNet(torch.nn.Module):
         for n in x.size()[1:]:
             first_fc_in_features *= n
 
-        self.head = torch.nn.Sequential(
-            torch.nn.Linear(first_fc_in_features, 128),
-            torch.nn.ReLU(),
-            torch.nn.Dropout(p=0.4),
-            torch.nn.Linear(128, num_classes)
-        )
+        self.head = torch.nn.Sequential(OrderedDict([
+            ('fc1', torch.nn.Linear(first_fc_in_features, 128)),
+            ('relu1', torch.nn.ReLU()),
+            ('drop3', torch.nn.Dropout(p=0.4)),
+            ('fc2', torch.nn.Linear(128, num_classes)),
+            ('softmax', torch.nn.Softmax())
+        ]))
+
+        if weights_path is not None:
+            weights = torch.load(weights_path)
+            if load_body_weights:
+                self.body.load_state_dict(weights["body"])
+            elif load_head_weights:
+                self.head.load_state_dict(weights["head"])
 
     def forward(self, x):
         x = self.body(x)
