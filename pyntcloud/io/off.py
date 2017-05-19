1b0ecfd51dc007ae2c2ee2933e5c6689ff6be4ff
David de la Iglesia Castro <daviddelaiglesiacastro@gmail.com>
2017-05-19 20:26:38 +0200

    fixes in off
    - Support for color off
    - pd.read_csv nazi stuffs

--- pyntcloud/io/off.py
+++ pyntcloud/io/off.py
@@ -1,15 +1,16 @@
 import pandas as pd
-
+import numpy as np
 
 def read_off(filename):
 
     with open(filename) as off:
 
-        if "OFF" not in off.readline():
+        first_line = off.readline()
+        if "OFF" not in first_line:
             raise ValueError('The file does not start whith the word OFF')
+        color = True if "C" in first_line else False
 
         count = 1
-
         for line in off:
             count += 1
             if line.startswith("#"):
@@ -21,10 +22,19 @@ def read_off(filename):
                 break
 
         data = {}
+        point_names = ["x", "y", "z"]
+        if color:
+            point_names.extend(["red", "green", "blue"])
 
         data["points"] = pd.read_csv(filename, sep=" ", header=None, engine="python",
                                      skiprows=count, skipfooter=n_faces,
-                                     names=["x", "y", "z"])
+                                     names=point_names, index_col=False)
+        for n in ["x", "y", "z"]:
+             data["points"][n] = data["points"][n].astype(np.float32)
+
+        if color:
+            for n in ["red", "green", "blue"]:
+                data["points"][n] = data["points"][n].astype(np.uint8)
 
         data["mesh"] = pd.read_csv(filename, sep=" ", header=None, engine="python",
                                    skiprows=(count + n_points), usecols=[1, 2, 3],
