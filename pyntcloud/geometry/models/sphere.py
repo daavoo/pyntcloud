97d9e06e3905a7059f142d18c1b3bc62b575fb8d
David de la Iglesia Castro <daviddelaiglesiacastro@gmail.com>
2017-06-14 12:49:53 +0200

    sphere least squares fit

--- pyntcloud/geometry/models/sphere.py
+++ pyntcloud/geometry/models/sphere.py
@@ -58,6 +58,36 @@ class Sphere(GeometryModel):
         self.center = np.array([cx, cy, cz])
         self.radius = np.sqrt(np.dot(self.center, self.center) - (m15 / m11))
 
+    def from_point_cloud(self, points):
+        """
+        Least Squares fit.
+
+        Parameters
+        ----------
+        points: (N, 3) ndarray
+        """
+        spX = points[:, 0]
+        spY = points[:, 1]
+        spZ = points[:, 2]
+
+        A = np.zeros((len(spX), 4))
+        A[:, 0] = spX * 2
+        A[:, 1] = spY * 2
+        A[:, 2] = spZ * 2
+        A[:, 3] = 1
+
+        #   Assemble the f matrix
+        f = np.zeros((len(spX), 1))
+        f[:, 0] = (spX * spX) + (spY * spY) + (spZ * spZ)
+        center, residules, rank, singval = np.linalg.lstsq(A, f)
+
+        #   solve for the radius
+        t = (center[0] * center[0]) + (center[1] * center[1]) + \
+            (center[2] * center[2]) + center[3]
+
+        self.center = center[:3].T[0]
+        self.radius = np.sqrt(t)
+
     def get_projections(self, points, only_distances=False):
         vectors = points - self.center
         lengths = np.linalg.norm(vectors, axis=1)
