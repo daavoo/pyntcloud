11b2ca14c79a04a30ebce331b2400bdbc986ee11
David de la Iglesia Castro <daviddelaiglesiacastro@gmail.com>
2017-06-15 13:07:05 +0200

    add cartesian_to_spherical to geomtry.coord_systems
    - Include optional kwarg degrees in all functions

--- pyntcloud/geometry/coord_systems.py
+++ pyntcloud/geometry/coord_systems.py
@@ -1,7 +1,7 @@
 import numpy as np
 
 
-def spherical_to_cartesian(radial, azimuthal, polar):
+def spherical_to_cartesian(radial, azimuthal, polar, degrees=True):
     """
     Convert spherical coordinates (r, theta, phi) to cartesian (x, y, z).
 
@@ -9,10 +9,12 @@ def spherical_to_cartesian(radial, azimuthal, polar):
     ----------
     radial: (N,) ndarray
         'r'. Radial distance.
-    azimutal: (N,) ndarray
-        'theta'. Azimutal angle.
+    azimuthal: (N,) ndarray
+        'theta'. Azimuthal angle.
     polar: (N,) ndarray
         'phi'. Polar angle.
+    degrees: bool, optional
+        If True, azimuthal and polar will be assumed to be in degrees.
 
     Returns
     -------
@@ -23,6 +25,10 @@ def spherical_to_cartesian(radial, azimuthal, polar):
     -----
     Use notation of mathematical systems, NOT physics.
     """
+    if degrees:
+        azimuthal = np.deg2rad(azimuthal)
+        polar = np.deg2rad(polar)
+
     sin_theta = np.sin(azimuthal)
     cos_theta = np.cos(azimuthal)
     sin_phi = np.sin(polar)
@@ -35,3 +41,44 @@ def spherical_to_cartesian(radial, azimuthal, polar):
     xyz[:, 2] = radial * cos_phi
 
     return xyz
+
+
+def cartesian_to_spherical(xyz, degrees=True):
+    """
+    Convert cartesian coordinates (x, y, z) to spherical (r, theta, phi).
+
+    Parameters
+    ----------
+    xyz: (N, 3) ndarray
+        Corresponding cartesian coordiantes.
+    degrees: bool, optional
+        If True, azimuthal and polar will be returned in degrees.
+
+    Returns
+    -------
+    radial: (N,) ndarray
+        'r'. Radial distance.
+    azimuthal: (N,) ndarray
+        'theta'. Azimuthal angle.
+    polar: (N,) ndarray
+        'phi'. Polar angle.
+
+    Notes
+    -----
+    Use notation of mathematical systems, NOT physics.
+    """
+    x = xyz[:, 0]
+    y = xyz[:, 1]
+    z = xyz[:, 2]
+
+    radial = np.sqrt((x * x) + (y * y) + (z * z))
+
+    azimuthal = np.arctan(y / x)
+
+    polar = np.arctan(np.sqrt((x * x) + (y * y)) / z)
+
+    if degrees:
+        azimuthal = np.rad2deg(azimuthal)
+        polar = np.rad2deg(polar)
+
+    return radial, azimuthal, polar
