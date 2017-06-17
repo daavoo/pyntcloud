2daa2a9fc1e7e44d77b6d3cc9ca2b57c64e4a941
David de la Iglesia Castro <daviddelaiglesiacastro@gmail.com>
2017-06-17 13:56:03 +0200

    add test_cartesian_to_spherical

--- test/test_geometry.py
+++ test/test_geometry.py
@@ -8,7 +8,7 @@ from pyntcloud.geometry.coord_systems import (
 def test_spherical_to_cartesian():
     expected = np.array([[0.5, 0.5, 0.7071], [0.5, -0.5, -0.7071]])
 
-    data = np.array([[1, 45, 45], [-1, -45, -45]], dtype=np.float32)
+    data = np.array([[1, 45, 45], [1, -45, 135]], dtype=np.float32)
     result = spherical_to_cartesian(data[:, 0], data[:, 1], data[:, 2])
 
     assert np.all(np.isclose(result, expected))
@@ -16,5 +16,28 @@ def test_spherical_to_cartesian():
     data[:, 1:] = np.deg2rad(data[:, 1:])
 
     result = spherical_to_cartesian(data[:, 0], data[:, 1], data[:, 2], degrees=False)
+
+    assert np.all(np.isclose(result, expected))
+
+
+def test_cartesian_to_spherical():
+    expected = np.array([[1, 45, 45], [1, -45, 135]], dtype=np.float32)
+
+    data = np.array([[0.5, 0.5, 0.7071], [0.5, -0.5, -0.7071]])
+
+    result = np.zeros_like(expected)
+    radial, azimuthal, polar = cartesian_to_spherical(data)
+    result[:, 0] = radial
+    result[:, 1] = azimuthal
+    result[:, 2] = polar
+
+    assert np.all(np.isclose(result, expected))
+
+    expected[:, 1:] = np.deg2rad(expected[:, 1:])
+
+    radial, azimuthal, polar = cartesian_to_spherical(data, degrees=False)
+    result[:, 0] = radial
+    result[:, 1] = azimuthal
+    result[:, 2] = polar
 
     assert np.all(np.isclose(result, expected))
