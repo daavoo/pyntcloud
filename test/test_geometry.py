4e4bc4dc0131b3dabe0aa8deb3fbc052c70882d2
David de la Iglesia Castro <daviddelaiglesiacastro@gmail.com>
2017-06-19 20:39:52 +0200

    add test for cylindrical coordinates
    cartesian_to_cylindrical
    cylindrical_to_cartesian

--- test/test_geometry.py
+++ test/test_geometry.py
@@ -1,7 +1,9 @@
 import numpy as np
 from pyntcloud.geometry.coord_systems import (
     spherical_to_cartesian,
-    cartesian_to_spherical
+    cartesian_to_spherical,
+    cylindrical_to_cartesian,
+    cartesian_to_cylindrical,
 )
 
 
@@ -39,5 +41,43 @@ def test_cartesian_to_spherical():
     result[:, 0] = radial
     result[:, 1] = azimuthal
     result[:, 2] = polar
+
+    assert np.all(np.isclose(result, expected))
+
+
+def test_cylindrical_to_cartesian():
+    expected = np.array([[0.5, 0.5, 0.7071], [0.5, -0.5, -0.7071]])
+
+    data = np.array([[0.7071, 45, 0.7071], [0.7071, -45, -0.7071]], dtype=np.float32)
+    result = cylindrical_to_cartesian(data[:, 0], data[:, 1], data[:, 2])
+
+    assert np.all(np.isclose(result, expected))
+
+    data[:, 1] = np.deg2rad(data[:, 1])
+
+    result = cylindrical_to_cartesian(data[:, 0], data[:, 1], data[:, 2], degrees=False)
+
+    assert np.all(np.isclose(result, expected))
+
+
+def test_cartesian_to_cylindrical():
+    expected = np.array([[0.7071, 45, 0.7071], [0.7071, -45, -0.7071]], dtype=np.float32)
+
+    data = np.array([[0.5, 0.5, 0.7071], [0.5, -0.5, -0.7071]])
+
+    result = np.zeros_like(expected)
+    radial, angular, height = cartesian_to_cylindrical(data)
+    result[:, 0] = radial
+    result[:, 1] = angular
+    result[:, 2] = height
+
+    assert np.all(np.isclose(result, expected))
+
+    expected[:, 1] = np.deg2rad(expected[:, 1])
+
+    radial, angular, height = cartesian_to_cylindrical(data, degrees=False)
+    result[:, 0] = radial
+    result[:, 1] = angular
+    result[:, 2] = height
 
     assert np.all(np.isclose(result, expected))
