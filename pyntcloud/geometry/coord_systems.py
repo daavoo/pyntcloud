c8eed34ad2e07c9eae53af692ff5872d0251c906
David de la Iglesia Castro <daviddelaiglesiacastro@gmail.com>
2017-06-19 20:40:23 +0200

    implement cylindrical coordinates
    cylindrical_to_cartesian
    cartesian_to_cylindrical

--- pyntcloud/geometry/coord_systems.py
+++ pyntcloud/geometry/coord_systems.py
@@ -82,3 +82,88 @@ def cartesian_to_spherical(xyz, degrees=True):
         polar = np.rad2deg(polar)
 
     return radial, azimuthal, polar
+
+
+def cylindrical_to_cartesian(radial, angular, height, degrees=True):
+    """
+    Convert cylindrical coordinates (ro, phi, zeta) to cartesian (x, y, z).
+
+    Parameters
+    ----------
+    radial: (N,) ndarray
+        'ro'. Radial distance.
+    angular: (N,) ndarray
+        'phi'. Angular position.
+    height: (N,) ndarray
+        'zeta'. Altitude.
+    degrees: bool, optional
+        If True, angular will be assumed to be in degrees.
+
+    Returns
+    -------
+    xyz: (N, 3) ndarray
+        Corresponding cartesian coordiantes.
+
+    Notes
+    -----
+    The polar axis in the cylindrical system corresponds to the 'x' axis in the
+    cartesian system.
+
+    The longitudinal axis corresponds to the 'z' axis.
+    """
+    if degrees:
+        angular = np.deg2rad(angular)
+
+    sin_phi = np.sin(angular)
+    cos_phi = np.cos(angular)
+
+    xyz = np.empty((radial.shape[0], 3), dtype=np.float32)
+
+    xyz[:, 0] = radial * cos_phi
+    xyz[:, 1] = radial * sin_phi
+    xyz[:, 2] = height
+
+    return xyz
+
+
+def cartesian_to_cylindrical(xyz, degrees=True):
+    """
+    Convert cartesian coordinates (x, y, z) to spherical cylindrical (ro, phi, zeta).
+
+    Parameters
+    ----------
+    xyz: (N, 3) ndarray
+        Corresponding cartesian coordiantes.
+    degrees: bool, optional
+        If True, azimuthal and polar will be returned in degrees.
+
+    Returns
+    -------
+    radial: (N,) ndarray
+        'ro'. Radial distance.
+    angular: (N,) ndarray
+        'phi'. Angular position.
+    height: (N,) ndarray
+        'zeta'. Altitude.
+
+    Notes
+    -----
+    The polar axis in the cylindrical system corresponds to the 'x' axis in the
+    cartesian system.
+
+    The longitudinal axis corresponds to the 'z' axis.
+    """
+    x = xyz[:, 0]
+    y = xyz[:, 1]
+    z = xyz[:, 2]
+
+    radial = np.sqrt((x * x) + (y * y))
+
+    angular = np.arctan(y / x)
+
+    height = z
+
+    if degrees:
+        angular = np.rad2deg(angular)
+
+    return radial, angular, height
