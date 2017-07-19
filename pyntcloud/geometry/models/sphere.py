bc2c7195d73157cd05c808806a2eb85007aef1c9
David de la Iglesia Castro <daviddelaiglesiacastro@gmail.com>
2017-07-19 11:27:17 +0200

    add create_sphere to geometry.models.sphere

--- pyntcloud/geometry/models/sphere.py
+++ pyntcloud/geometry/models/sphere.py
@@ -1,4 +1,5 @@
 import numpy as np
+import pandas as pd
 from .base import GeometryModel
 
 
@@ -97,3 +98,66 @@ class Sphere(GeometryModel):
         scales = self.radius / lengths
         projections = (scales[:, None] * vectors) + self.center
         return distances, projections
+
+
+def create_sphere(center=[0, 0, 0], radius=1, n_points=100):
+    """
+    Generate a point cloud with the shape of a sphere.
+
+    Parameters
+    ----------
+    center: (3,) array-like
+        Default: [0, 0, 0]
+        These will be the coordinates of the centroid of the sphere.abs
+    radius: int or float
+        Default: 1
+    n_points: int
+        Default: 100
+        The output PyntCloud will have this number of points.abs
+        The higher this number is, the better the sphere surface
+        is represented.
+
+    Return
+    ------
+    sphere: pd.DataFrame
+        A DataFrame suitable to be used for PyntCloud(sphere) instantiation.
+    """
+    np_axis = round(np.sqrt(n_points - 2), 0) + 1
+
+    index = np.arange(0, np.square(np_axis) + 2, 1)
+    sphere = pd.DataFrame(np.zeros([np.size(index, 0), 3]),
+                       index=index, columns=['x', 'y', 'z'])
+
+    zmin = center[2] - radius
+    zmax = center[2] + radius
+
+    tick = 2 * np.pi / (np_axis + 1)
+    xx = np.arange(tick, 2 * np.pi, tick)
+    yy = np.arange(tick, 2 * np.pi, tick)
+
+    u, v = np.meshgrid(xx, yy)
+    x = radius * np.cos(u) * np.sin(v) + center[0]
+    y = radius * np.sin(u) * np.sin(v) + center[1]
+    z = radius * np.cos(v) + center[2]
+
+    X = np.arange(0, 1)
+    Y = np.arange(0, 1)
+    Z = np.arange(0, 1)
+
+    X[0] = center[0]
+    Y[0] = center[1]
+    Z[0] = zmin
+
+    X = np.append(X, x.reshape([1, np.size(x)]))
+    Y = np.append(Y, y.reshape([1, np.size(y)]))
+    Z = np.append(Z, z.reshape([1, np.size(z)]))
+
+    X = np.append(X, center[0])
+    Y = np.append(Y, center[1])
+    Z = np.append(Z, zmax)
+
+    sphere.x = np.round(X.T, 4).astype(np.float32)
+    sphere.y = np.round(Y.T, 4).astype(np.float32)
+    sphere.z = np.round(Z.T, 4).astype(np.float32)
+
+    return sphere
