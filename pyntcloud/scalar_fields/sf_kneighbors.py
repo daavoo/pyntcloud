93f5d67977251f61915a9d08504d2e660b36d34e
David de la Iglesia Castro <daviddelaiglesiacastro@gmail.com>
2017-08-23 09:12:27 +0200

    first commit scalar field normals

--- pyntcloud/scalar_fields/sf_kneighbors.py
+++ pyntcloud/scalar_fields/sf_kneighbors.py
@@ -1,4 +1,8 @@
 import numpy as np
+
+from scipy.sparse import csr_matrix
+from scipy.sparse.csgraph import minimum_spanning_tree, depth_first_order
+
 from .base import ScalarField
 from ..utils.array import cov3D
 
@@ -13,14 +17,14 @@ class ScalarField_KNeighbors(ScalarField):
 
     def __init__(self, pyntcloud, k_neighbors):
         super().__init__(pyntcloud)
-        self.k_neighbors = k_neighbors
+        self.k_neighbors_idx = k_neighbors
 
     def extract_info(self):
-        self.k_neighbors = self.pyntcloud.xyz[self.k_neighbors]
+        self.k_neighbors = self.pyntcloud.xyz[self.k_neighbors_idx]
 
 
 class EigenValues(ScalarField_KNeighbors):
-    """ Compute the eigen values of each point's neighbourhood
+    """Compute the eigen values of each point's neighbourhood.
     """
     def compute(self):
         cov = cov3D(self.k_neighbors)
@@ -41,7 +45,7 @@ class EigenValues(ScalarField_KNeighbors):
 
 
 class EigenDecomposition(ScalarField_KNeighbors):
-    """ Compute the eigen decomposition of each point's neighbourhood
+    """Compute the eigen decomposition of each point's neighbourhood.
     """
     def compute(self):
         cov = cov3D(self.k_neighbors)
@@ -70,17 +74,45 @@ class EigenDecomposition(ScalarField_KNeighbors):
 
 
 class Normals(ScalarField_KNeighbors):
-    """ Compute normals using k neighbors.
+    """Compute normals using SVD and refine orientation with Riemanian Graph.
     """
     def compute(self):
         cov = cov3D(self.k_neighbors)
         u, s, v = np.linalg.svd(cov)
 
-        unoriented_normals = u[:, :, -1]
+        normals = u[:, :, -1]
 
-        nx = unoriented_normals[:, 0]
-        ny = unoriented_normals[:, 1]
-        nz = unoriented_normals[:, 2]
+        # Orient normals as in "Surface Reconstruction from Unorganized Points"
+
+        max_z = self.pyntcloud.xyz.argmax(0)[-1]
+        if normals[max_z, 2] < 0:
+            normals[max_z] = -normals[max_z]
+        
+        # Dot product between each point's normal and the normals of it's neighbours
+        dot3D = 1 - abs(np.einsum("ij, ikj -> ik",
+                                  normals,
+                                  normals[self.k_neighbors_idx]))
+
+        n = self.pyntcloud.xyz.shape[0]
+        graph = np.zeros((n, n), dtype=np.float32)
+        for i in range(n):
+            graph[i, self.k_neighbors_idx[i]] = dot3D[i]
+
+        MST = minimum_spanning_tree(csr_matrix(graph))
+        DFO = depth_first_order(MST, max_z,
+                                directed=False,
+                                return_predecessors=False)
+        """
+        for i in range(1, len(DFO)):
+            n1 = normals[DFO[i - 1]]
+            n2 = normals[DFO[i]]
+            if np.dot(n1, n2) < 0:
+                normals[DFO[i]] *= -1
+        """
+        nx = normals[:, 0]
+        ny = normals[:, 1]
+        nz = normals[:, 2]
+
         k = self.k_neighbors.shape[1]
         self.to_be_added["nx({})".format(k)] = nx
         self.to_be_added["ny({})".format(k)] = ny
