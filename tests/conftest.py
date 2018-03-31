dd79fdca4749a9acba3f0728f04f6f4b83f6b8fe
David de la Iglesia Castro <daviddelaiglesiacastro@gmail.com>
2018-03-31 16:47:59 +0200

    Add diamond fixture to tests/conftest

--- tests/conftest.py
+++ tests/conftest.py
@@ -31,3 +31,34 @@ def pyntcloud_with_kdtree(simple_pyntcloud):
     simple_pyntcloud.add_structure("kdtree")
     return simple_pyntcloud
 
+
+@pytest.fixture()
+def diamond():
+    points = pd.DataFrame(
+        data={
+            "x": np.array([0.5, 0., 0.5, 1., 0.5, 0.5], dtype=np.float32),
+            "y": np.array([0., 0.5, 0.5, 0.5, 1., 0.5], dtype=np.float32),
+            "z": np.array([0.5, 0.5, 0., 0.5, 0.5, 1.], dtype=np.float32),
+            "red": np.array([255, 255, 0, 255, 255, 0], dtype=np.uint8),
+            "green": np.array([0, 0, 255, 0, 0, 0], dtype=np.uint8),
+            "blue": np.array([0, 0, 0, 0, 0, 255], dtype=np.uint8),
+            "nx": np.array([0., -1., 0., 1., 0., 0.], dtype=np.float32),
+            "ny": np.array([-1., 0., 0., 0., 1., 0.], dtype=np.float32),
+            "nz": np.array([0., 0., -1., 0., 0., 1.], dtype=np.float32)
+        })
+
+    vertices = np.array([
+        [0, 1, 2],
+        [0, 3, 2],
+        [3, 4, 2],
+        [4, 1, 2],
+        [0, 1, 5],
+        [0, 3, 5],
+        [3, 4, 5],
+        [4, 1, 5]])
+
+    mesh = pd.DataFrame(
+        data=vertices,
+        columns=["v1", "v2", "v3"])
+
+    return PyntCloud(points=points, mesh=mesh)
