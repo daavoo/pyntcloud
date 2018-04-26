1ff73746824af11e540ce43855041fb055b76ceb
David de la Iglesia Castro <daviddelaiglesiacastro@gmail.com>
2018-04-26 19:29:19 +0200

    Renaming and key-word only arguments

--- pyntcloud/scalar_fields/voxelgrid.py
+++ pyntcloud/scalar_fields/voxelgrid.py
@@ -2,48 +2,48 @@ import numpy as np
 from .base import ScalarField
 
 
-class ScalarField_Voxelgrid(ScalarField):
+class VoxelgridScalarField(ScalarField):
 
-    def __init__(self, pyntcloud, voxelgrid):
-        super().__init__(pyntcloud)
-        self.voxelgrid = voxelgrid
+    def __init__(self, *, pyntcloud, voxelgrid_id):
+        super().__init__(pyntcloud=pyntcloud)
+        self.voxelgrid_id = voxelgrid_id
 
     def extract_info(self):
-        self.voxelgrid = self.pyntcloud.structures[self.voxelgrid]
+        self.voxelgrid = self.pyntcloud.structures[self.voxelgrid_id]
 
 
-class VoxelX(ScalarField_Voxelgrid):
+class VoxelX(VoxelgridScalarField):
     """Voxel index along x axis."""
     def compute(self):
-        name = "{}({})".format("voxel_x", self.voxelgrid.id)
+        name = "{}({})".format("voxel_x", self.voxelgrid_id)
         self.to_be_added[name] = self.voxelgrid.voxel_x
 
 
-class VoxelY(ScalarField_Voxelgrid):
+class VoxelY(VoxelgridScalarField):
     """Voxel index along y axis."""
     def compute(self):
-        name = "{}({})".format("voxel_y", self.voxelgrid.id)
+        name = "{}({})".format("voxel_y", self.voxelgrid_id)
         self.to_be_added[name] = self.voxelgrid.voxel_y
 
 
-class VoxelZ(ScalarField_Voxelgrid):
+class VoxelZ(VoxelgridScalarField):
     """Voxel index along z axis."""
     def compute(self):
-        name = "{}({})".format("voxel_z", self.voxelgrid.id)
+        name = "{}({})".format("voxel_z", self.voxelgrid_id)
         self.to_be_added[name] = self.voxelgrid.voxel_z
 
 
-class VoxelN(ScalarField_Voxelgrid):
+class VoxelN(VoxelgridScalarField):
     """Voxel index in 3D array using 'C' order."""
     def compute(self):
-        name = "{}({})".format("voxel_n", self.voxelgrid.id)
+        name = "{}({})".format("voxel_n", self.voxelgrid_id)
         self.to_be_added[name] = self.voxelgrid.voxel_n
 
 
-class EuclideanClusters(ScalarField_Voxelgrid):
+class EuclideanClusters(VoxelgridScalarField):
     """Assing corresponding cluster to each point inside each voxel."""
     def compute(self):
-        name = "{}({})".format("clusters", self.voxelgrid.id)
+        name = "{}({})".format("clusters", self.voxelgrid_id)
 
         to_be_processed = np.zeros(self.voxelgrid.n_voxels, dtype=bool)
         to_be_processed[np.unique(self.voxelgrid.voxel_n)] = True
