2b43d700e4dcc1edb5872ea32e1256a3b588fee7
David de la Iglesia Castro <daviddelaiglesiacastro@gmail.com>
2018-05-20 13:59:42 +0200

    Adapt test_voxelgrid_samplers to new VoxelGrid interface

--- tests/unit/samplers/test_voxelgrid_samplers.py
+++ tests/unit/samplers/test_voxelgrid_samplers.py
@@ -8,23 +8,25 @@ from pyntcloud.samplers import (
 from pyntcloud.utils.array import point_in_array_2D
 
 
-@pytest.mark.parametrize("x_y_z,expected_n,expected_point", [
+@pytest.mark.parametrize("n_x, n_y, n_z,expected_n,expected_point", [
     (
-        [2, 2, 2],
+        2, 2, 2,
         2,
         [0.25, 0.25, 0.25]
     ),
     (
-        [2, 1, 1],
+        2, 1, 1,
         2,
         [0.25, 0.5, 0.5]
     )
 ])
 @pytest.mark.usefixtures("simple_pyntcloud")
-def test_VoxelgridCentersSampler_expected_values(simple_pyntcloud, x_y_z, expected_n, expected_point):
+def test_VoxelgridCentersSampler_expected_values(simple_pyntcloud, n_x, n_y, n_z, expected_n, expected_point):
     voxelgrid_id = simple_pyntcloud.add_structure(
         "voxelgrid",
-        x_y_z=x_y_z)
+        n_x=n_x,
+        n_y=n_y,
+        n_z=n_z)
     sampler = VoxelgridCentersSampler(
         pyntcloud=simple_pyntcloud,
         voxelgrid_id=voxelgrid_id)
@@ -35,23 +37,25 @@ def test_VoxelgridCentersSampler_expected_values(simple_pyntcloud, x_y_z, expect
     assert point_in_array_2D(expected_point, sample.values)
 
 
-@pytest.mark.parametrize("x_y_z,expected_n,expected_point", [
+@pytest.mark.parametrize("n_x, n_y, n_z,expected_n,expected_point", [
     (
-        [2, 2, 2],
+        2, 2, 2,
         2,
         [0.2, 0.2, 0.2]
     ),
     (
-        [2, 1, 1],
+        2, 1, 1,
         2,
         [0.2, 0.2, 0.2]
     )
 ])
 @pytest.mark.usefixtures("simple_pyntcloud")
-def test_VoxelgridCentroidsSampler_expected_values(simple_pyntcloud, x_y_z, expected_n, expected_point):
+def test_VoxelgridCentroidsSampler_expected_values(simple_pyntcloud, n_x, n_y, n_z, expected_n, expected_point):
     voxelgrid_id = simple_pyntcloud.add_structure(
         "voxelgrid",
-        x_y_z=x_y_z)
+        n_x=n_x,
+        n_y=n_y,
+        n_z=n_z)
     sampler = VoxelgridCentroidsSampler(
         pyntcloud=simple_pyntcloud,
         voxelgrid_id=voxelgrid_id)
@@ -62,31 +66,33 @@ def test_VoxelgridCentroidsSampler_expected_values(simple_pyntcloud, x_y_z, expe
     assert point_in_array_2D(expected_point, sample.values)
 
 
-@pytest.mark.parametrize("x_y_z,n_points,expected_n,expected_point", [
+@pytest.mark.parametrize("n_x, n_y, n_z,n_points,expected_n,expected_point", [
     (
-        [2, 2, 2],
+        2, 2, 2,
         1,
         2,
         [0.2, 0.2, 0.2]
     ),
     (
-        [2, 2, 2],
+        2, 2, 2,
         2,
         4,
         [0.1, 0.1, 0.1]
     ),
     (
-        [2, 1, 1],
+        2, 1, 1,
         1,
         2,
         [0.5, 0.5, 0.5]
     )
 ])
 @pytest.mark.usefixtures("simple_pyntcloud")
-def test_VoxelgridNearestSampler_expected_values(simple_pyntcloud, x_y_z, n_points, expected_n, expected_point):
+def test_VoxelgridNearestSampler_expected_values(simple_pyntcloud, n_x, n_y, n_z, n_points, expected_n, expected_point):
     voxelgrid_id = simple_pyntcloud.add_structure(
         "voxelgrid",
-        x_y_z=x_y_z)
+        n_x=n_x,
+        n_y=n_y,
+        n_z=n_z)
     sampler = VoxelgridNearestSampler(
         pyntcloud=simple_pyntcloud,
         voxelgrid_id=voxelgrid_id,
