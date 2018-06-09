1e836c3c4c9bce0fd9b0bf09477de516a49e61f8
David de la Iglesia Castro <daviddelaiglesiacastro@gmail.com>
2018-06-09 20:16:46 +0200

    Add test for VoxelgridHighestSampler
    And test case to check if voxelgrid nearest keeps the original scalar fields

--- tests/unit/samplers/test_voxelgrid_samplers.py
+++ tests/unit/samplers/test_voxelgrid_samplers.py
@@ -3,7 +3,8 @@ import pytest
 from pyntcloud.samplers import (
     VoxelgridCentersSampler,
     VoxelgridCentroidsSampler,
-    VoxelgridNearestSampler
+    VoxelgridNearestSampler,
+    VoxelgridHighestSampler
 )
 from pyntcloud.utils.array import point_in_array_2D
 
@@ -101,5 +102,46 @@ def test_VoxelgridNearestSampler_expected_values(simple_pyntcloud, n_x, n_y, n_z
 
     sample = sampler.compute()
     assert len(sample) == expected_n
-    assert point_in_array_2D(expected_point, sample.values)
+    assert point_in_array_2D(expected_point, sample.loc[:, ["x", "y", "z"]].values)
 
+
+def test_VoxelgridNearestSampler_keeps_original_scalar_fields(pyntcloud_with_rgb_and_normals):
+    voxelgrid_id = pyntcloud_with_rgb_and_normals.add_structure(
+        "voxelgrid")
+    sampler = VoxelgridNearestSampler(
+        pyntcloud=pyntcloud_with_rgb_and_normals,
+        voxelgrid_id=voxelgrid_id)
+    sampler.extract_info()
+
+    sample = sampler.compute()
+    assert "red" in sample
+
+
+@pytest.mark.parametrize("size_x,expected_n,expected_in,expected_not_in", [
+    (
+        0.1,
+        6,
+        [0., 0., 0.],
+        [1.2, 1.2, 1.2]
+    ),
+    (
+        0.2,
+        4,
+        [0.1, 0.1, 0.1],
+        [0.9, 0.9, 0.9]
+    )
+])
+@pytest.mark.usefixtures("simple_pyntcloud")
+def test_VoxelgridHighestSampler_expected_values(simple_pyntcloud, size_x, expected_n, expected_in, expected_not_in):
+    voxelgrid_id = simple_pyntcloud.add_structure(
+        "voxelgrid",
+        size_x=size_x)
+    sampler = VoxelgridHighestSampler(
+        pyntcloud=simple_pyntcloud,
+        voxelgrid_id=voxelgrid_id)
+    sampler.extract_info()
+
+    sample = sampler.compute()
+    assert len(sample) == expected_n
+    assert point_in_array_2D(expected_in, sample.loc[:, ["x", "y", "z"]].values)
+    assert not point_in_array_2D(expected_not_in, sample.loc[:, ["x", "y", "z"]].values)
