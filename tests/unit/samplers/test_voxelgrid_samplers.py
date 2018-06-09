eb1c4637d0f16a601ab81fc9db2717a5bb413a1d
David de la Iglesia Castro <daviddelaiglesiacastro@gmail.com>
2018-06-09 20:31:24 +0200

    Add test case for VoxelgridHighestSampler
    Check if keeps original scalar fields

--- tests/unit/samplers/test_voxelgrid_samplers.py
+++ tests/unit/samplers/test_voxelgrid_samplers.py
@@ -145,3 +145,15 @@ def test_VoxelgridHighestSampler_expected_values(simple_pyntcloud, size_x, expec
     assert len(sample) == expected_n
     assert point_in_array_2D(expected_in, sample.loc[:, ["x", "y", "z"]].values)
     assert not point_in_array_2D(expected_not_in, sample.loc[:, ["x", "y", "z"]].values)
+
+
+def test_VoxelgridHighestSampler_keeps_original_scalar_fields(pyntcloud_with_rgb_and_normals):
+    voxelgrid_id = pyntcloud_with_rgb_and_normals.add_structure(
+        "voxelgrid")
+    sampler = VoxelgridHighestSampler(
+        pyntcloud=pyntcloud_with_rgb_and_normals,
+        voxelgrid_id=voxelgrid_id)
+    sampler.extract_info()
+
+    sample = sampler.compute()
+    assert "red" in sample
