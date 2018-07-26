8d63d75858cc6df30ea19c55f0da955d8d46f53a
David de la Iglesia Castro <daviddelaiglesiacastro@gmail.com>
2018-07-26 21:54:26 +0200

    Refactor pythreejs backend to plot/pythreejs.py

--- pyntcloud/plot/pythreejs.py
+++ pyntcloud/plot/pythreejs.py
@@ -29,3 +29,27 @@ def get_polylines_pythreejs(polylines):
         lines.append(line)
 
     return lines
+
+
+def get_camera_pythreejs(centroid, xyz, width, height):
+    import pythreejs
+    camera = pythreejs.PerspectiveCamera(
+        fov=90,
+        aspect=width / height,
+        position=tuple(centroid + [0, abs(xyz.max(0)[1]), abs(xyz.max(0)[2]) * 1.5]),
+        up=[0, 0, 1])
+    camera.lookAt(tuple(centroid))
+    return camera
+
+def get_orbit_controls(camera, centroid):
+    import pythreejs
+    orbit_controls = pythreejs.OrbitControls(controlling=camera)
+    orbit_controls.target = tuple(centroid)
+    return orbit_controls
+
+def get_scene():
+    pass
+
+
+def plot_PyntCloud_pythreejs(PyntCloud):
+    pass
