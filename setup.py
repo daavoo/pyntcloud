1666df8c9fc662ed2539138eb5f5bcdabf51d89c
David de la Iglesia Castro <diglesia@gradiant.org>
2019-07-14 11:10:33 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,16 @@
 from setuptools import setup, find_packages
 
-version = '0.1.0'
+version = '0.0.1'
+
+with open("README.rst", "r") as fh:
+    long_description = fh.read()
 
 setup(
     name='pyntcloud',
     version=version,
     description='Python library for working with 3D point clouds.',
+    long_description=long_description,
+    long_description_content_type="text/x-rst",
     url='https://github.com/daavoo/pyntcloud',
     author='David de la Iglesia Castro',
     author_email='daviddelaiglesiacastro@gmail.com',
@@ -22,4 +27,9 @@ setup(
         'PLOT': ["ipython", "matplotlib", "pyvista"],
         'NUMBA': ["numba"]
     }
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ]
 )
