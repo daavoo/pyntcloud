8ffc49a9a7323c650248e015825441d150fd2032
David de la Iglesia Castro <daviddelaiglesiacastro@gmail.com>
2019-10-05 22:12:48 +0200

    Update base.py

--- pyntcloud/geometry/models/base.py
+++ pyntcloud/geometry/models/base.py
@@ -4,7 +4,7 @@ from abc import ABC, abstractmethod
 class GeometryModel(ABC):
 
     @abstractmethod
-    def from_k_points(self):
+    def from_k_points(self, points):
         pass
 
     @abstractmethod
