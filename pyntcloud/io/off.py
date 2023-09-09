161f0b471bc2c74fbbae1d1ca3fcf51eac75da27
Edoardo Bortolozzo <edoardo.bortolozzo@studenti.unipd.it>
2023-09-09 15:53:43 +0200

    FIXED `read_off` method from `off.py`
    ADDED comment parameter to skip comment rows and end of line comments
    CHANGED to c engine to increase drastically the performance
    ADDED n_rows and REMOVED skipfooter to use c engine
    ADDED dtype parameter to directly cast the element types
    SET points read_csv to off buffer: no skip of first lines needed

--- pyntcloud/io/off.py
+++ pyntcloud/io/off.py
@@ -8,7 +8,7 @@ def read_off(filename):
 
         first_line = off.readline()
         if "OFF" not in first_line:
-            raise ValueError('The file does not start whith the word OFF')
+            raise ValueError('The file does not start with the word OFF')
         color = True if "C" in first_line else False
 
         count = 1
@@ -24,20 +24,40 @@ def read_off(filename):
 
         data = {}
         point_names = ["x", "y", "z"]
+        point_types = {'x': np.float32, 'y': np.float32, 'z': np.float32}
+
         if color:
             point_names.extend(["red", "green", "blue"])
+            point_types = dict(point_types, **{'red': np.uint8, 'green': np.uint8, 'blue': np.uint8})
 
-        data["points"] = pd.read_csv(filename, sep=" ", header=None, engine="python",
-                                     skiprows=count, skipfooter=n_faces,
-                                     names=point_names, index_col=False)
-        for n in ["x", "y", "z"]:
-            data["points"][n] = data["points"][n].astype(np.float32)
+        data["points"] =    pd.read_csv(
+                                off,
+                                sep=" ",
+                                header=None,
+                                engine="c",
+                                n_rows=n_points,
+                                names=point_names,
+                                dtype=point_types,
+                                index_col=False,
+                                comment="#"
+                            )
+        
+        # for n in ["x", "y", "z"]:
+        #     data["points"][n] = data["points"][n].astype(np.float32)
 
-        if color:
-            for n in ["red", "green", "blue"]:
-                data["points"][n] = data["points"][n].astype(np.uint8)
+        # if color:
+        #     for n in ["red", "green", "blue"]:
+        #         data["points"][n] = data["points"][n].astype(np.uint8)
 
-        data["mesh"] = pd.read_csv(filename, sep=" ", header=None, engine="python",
-                                   skiprows=(count + n_points), usecols=[1, 2, 3],
-                                   names=["v1", "v2", "v3"])
+        data["mesh"] = pd.read_csv(
+                            filename,
+                            sep=" ",
+                            header=None,
+                            engine="c",
+                            skiprows=(count + n_points),
+                            n_rows=n_faces,
+                            usecols=[1, 2, 3],
+                            names=["v1", "v2", "v3"],
+                            comment="#"
+                        )
         return data
